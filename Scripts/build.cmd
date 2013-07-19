e554bc7d26ac47b912a83fe7a3421a8cf4c4359a
Andreas Larsen <anjdreas@gmail.com>
2013-07-19 23:46:31 +0200

    Added all target DLLs to nuget package. Renamed winrt45 project and references/texts to netcore45. Renamed Pcl to lowercase pcl.

--- Scripts/build.cmd
+++ Scripts/build.cmd
@@ -15,8 +15,10 @@ MKDIR %BUILD_DIR%
 
 copy .\%PackageName%.nuspec %BUILD_BASE_DIR%
 
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:Rebuild /p:OutDir=%BUILD_DIR%\ /p:Configuration=Release .\Src\UnitsNet\UnitsNet.net35.csproj
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:Rebuild /p:OutDir=%BUILD_DIR% /p:Configuration=Release .\Src\UnitsNet\UnitsNet.Pcl.csproj
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:Rebuild /p:OutDir=%BUILD_DIR% /p:Configuration=Release .\Src\UnitsNet\UnitsNet.net35.csproj
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:Rebuild /p:OutDir=%BUILD_DIR% /p:Configuration=Release .\Src\UnitsNet\UnitsNet.pcl.csproj
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:Rebuild /p:OutDir=%BUILD_DIR% /p:Configuration=Release .\Src\UnitsNet\UnitsNet.sl4.csproj
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:Rebuild /p:OutDir=%BUILD_DIR% /p:Configuration=Release .\Src\UnitsNet\UnitsNet.netcore45.csproj
 
 %NuGetExe% pack %BUILD_BASE_DIR%\%PackageName%.nuspec -Verbose -OutputDirectory "%BUILD_BASE_DIR%" -BasePath "%BUILD_BASE_DIR%"
 
