c4cfb7b8f49050c9c6a8955851f04db23de89b04
Andreas Larsen <anjdreas@gmail.com>
2013-07-19 08:57:31 +0200

    Added UnitsNet.net35 to nuget package.

--- Scripts/build.cmd
+++ Scripts/build.cmd
@@ -14,8 +14,8 @@ RMDIR /S /Q %BUILD_BASE_DIR%
 MKDIR %BUILD_DIR%
 
 copy .\%PackageName%.nuspec %BUILD_BASE_DIR%
-REM %SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild /t:Rebuild /p:OutDir=%BUILD_DIR%\ /p:Configuration=Release .\JetBrains.Annotations.NET.2.0.csproj
 
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:Rebuild /p:OutDir=%BUILD_DIR%\ /p:Configuration=Release .\Src\UnitsNet\UnitsNet.net35.csproj
 %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:Rebuild /p:OutDir=%BUILD_DIR% /p:Configuration=Release .\Src\UnitsNet\UnitsNet.Pcl.csproj
 
 %NuGetExe% pack %BUILD_BASE_DIR%\%PackageName%.nuspec -Verbose -OutputDirectory "%BUILD_BASE_DIR%" -BasePath "%BUILD_BASE_DIR%"
