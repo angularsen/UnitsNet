adb1d141e4b8a2ff5311ef41425383e9af5a5b48
Andreas Larsen <anjdreas@gmail.com>
2013-07-20 00:00:21 +0200

    Fixed nuget warning, changed -Verbose to -Verbosity detailed.

--- Scripts/build.cmd
+++ Scripts/build.cmd
@@ -20,7 +20,7 @@ copy .\%PackageName%.nuspec %BUILD_BASE_DIR%
 %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:Rebuild /p:OutDir=%BUILD_DIR% /p:Configuration=Release .\Src\UnitsNet\UnitsNet.sl4.csproj
 %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:Rebuild /p:OutDir=%BUILD_DIR% /p:Configuration=Release .\Src\UnitsNet\UnitsNet.netcore45.csproj
 
-%NuGetExe% pack %BUILD_BASE_DIR%\%PackageName%.nuspec -Verbose -OutputDirectory "%BUILD_BASE_DIR%" -BasePath "%BUILD_BASE_DIR%"
+%NuGetExe% pack %BUILD_BASE_DIR%\%PackageName%.nuspec -Verbosity detailed -OutputDirectory "%BUILD_BASE_DIR%" -BasePath "%BUILD_BASE_DIR%"
 
 popd
 pause
