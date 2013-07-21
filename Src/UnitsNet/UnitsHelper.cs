1050af281a766b0eba9dd532dee2bbd847481040
Andreas Larsen <anjdreas@gmail.com>
2013-07-22 00:16:52 +0200

    Added UnitsParser.

--- Src/UnitsNet/UnitsHelper.cs
+++ Src/UnitsNet/UnitsHelper.cs
@@ -33,36 +33,6 @@ namespace UnitsNet
         public const double Gravity = 9.80665;
 
         /// <summary>
-        ///     Returns a Unit based on the string representation for that unit.
-        ///     Example: Parse("kg"); or Parse("Kilogram");
-        /// </summary>
-        /// <returns>true if parse was successful</returns>
-        public static bool TryParse(string unitText, out Unit result, Unit fallback, CultureInfo culture = null)
-        {
-            if (String.IsNullOrEmpty(unitText))
-                throw new ArgumentException(
-                    "Invalid unit description. Should for instance be kg or Kilogram to represent Unit.Kilogram type.");
-
-            const StringComparison invariantIgnoreCase = StringComparison.OrdinalIgnoreCase;
-
-
-            // Iterate over all the unit types and look for matching names or abbreviations
-            foreach (Unit unitType in EnumUtils.GetEnumValues<Unit>())
-            {
-                if (
-                    unitText.Equals(UnitSystem.Create(culture).GetDefaultAbbreviation(unitType), invariantIgnoreCase) ||
-                    unitText.Equals(unitType.ToString(), invariantIgnoreCase))
-                {
-                    result = unitType;
-                    return true;
-                }
-            }
-
-            result = fallback;
-            return false;
-        }
- 
-        /// <summary>
         /// A general method for converting a value in a given metric unit domain to the respective value in a compatible unit domain,
         /// such as converting from milimeters to meters.
         /// </summary>
