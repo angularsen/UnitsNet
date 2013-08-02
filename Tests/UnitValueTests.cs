ceb685b3d3d902fa1107abf7c268aa2a5d14ca89
Andreas Larsen <anjdreas@gmail.com>
2013-08-02 02:06:33 +0200

    Added UnitValueTests.cs and TryConvertReturnsFalseOnIncompatibleUnits.

---
+++ Tests/UnitValueTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitsNet.Tests.net35
+{
+    [TestFixture]
+    public class UnitValueTests
+    {
+        [Test]
+        public void TryConvertReturnsFalseOnIncompatibleUnits()
+        {
+            var val = new UnitValue(1, Unit.Meter);
+            double newValue;
+            Assert.False(val.TryConvert(Unit.Second, out newValue));
+        }
+    }
+}
