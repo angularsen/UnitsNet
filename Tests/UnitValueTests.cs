2037225369b130cba96e9530d4e8a0665368b649
Andreas Larsen <anjdreas@gmail.com>
2013-10-30 20:22:50 +0100

    Added missing test for Force units in UnitValueTests.cs.

--- Tests/UnitValueTests.cs
+++ Tests/UnitValueTests.cs
@@ -10,10 +10,11 @@ namespace UnitsNet.Tests.net35
         {
             double newValue;
             Assert.False(new UnitValue(1, Unit.Meter).TryConvert(Unit.Second, out newValue));
-            Assert.False(new UnitValue(1, Unit.Kilogram).TryConvert(Unit.Second, out newValue));
-            Assert.False(new UnitValue(1, Unit.Pascal).TryConvert(Unit.Second, out newValue));
             Assert.False(new UnitValue(1, Unit.SquareMeter).TryConvert(Unit.Second, out newValue));
             Assert.False(new UnitValue(1, Unit.CubicMeter).TryConvert(Unit.Second, out newValue));
+            Assert.False(new UnitValue(1, Unit.Newton).TryConvert(Unit.Second, out newValue));
+            Assert.False(new UnitValue(1, Unit.Pascal).TryConvert(Unit.Second, out newValue));
+            Assert.False(new UnitValue(1, Unit.Kilogram).TryConvert(Unit.Second, out newValue));
             Assert.False(new UnitValue(1, Unit.Degree).TryConvert(Unit.Second, out newValue));
         }
     }
