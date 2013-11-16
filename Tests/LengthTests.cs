7178b8f7d6eecd7aeca5db448108cfc298dba4e3
Andreas Larsen <anjdreas@gmail.com>
2013-11-16 22:45:54 +0100

    Re-implemented LengthTests.cs using generated LengthTestsBase.

--- Tests/LengthTests.cs
+++ Tests/LengthTests.cs
@@ -1,151 +1,83 @@
 ﻿// Disable build warning CS1718: Comparison made to same variable; did you mean to compare something else?
+
 #pragma warning disable 1718
 
-using System;
 using NUnit.Framework;
+using System;
 
 namespace UnitsNet.Tests.net35
 {
     [TestFixture]
-    public class LengthTests
+    public class LengthTests : LengthTestsBase
     {
-        private const double Delta = 1E-5;
-
-        [Test]
-        public void MetersToDistanceUnits()
+        protected override double Delta
         {
-            Length meter = Length.FromMeters(1);
-
-            Assert.AreEqual(1E-3, meter.Kilometers, Delta);
-            Assert.AreEqual(1, meter.Meters, Delta);
-            Assert.AreEqual(1E1, meter.Decimeters, Delta);
-            Assert.AreEqual(1E2, meter.Centimeters, Delta);
-            Assert.AreEqual(1E3, meter.Millimeters, Delta);
-            Assert.AreEqual(1E6, meter.Micrometers, Delta);
-            Assert.AreEqual(1E9, meter.Nanometers, Delta);
-
-            Assert.AreEqual(0.000621371, meter.Miles, Delta);
-            Assert.AreEqual(1.09361, meter.Yards, Delta);
-            Assert.AreEqual(3.28084, meter.Feet, Delta);
-            Assert.AreEqual(39.37007874, meter.Inches, Delta);
-            Assert.AreEqual(39370078.74015748, meter.Microinches, Delta);
-            Assert.AreEqual(39370.07874015, meter.Mils, Delta);
+            get { return 1E-5; }
         }
 
-        [Test]
-        public void DistanceUnitsRoundTrip()
+        protected override double CentimetersInOneMeter
         {
-            Length meter = Length.FromMeters(1);
-
-            Assert.AreEqual(1, Length.FromKilometers(meter.Kilometers).Meters, Delta);
-            Assert.AreEqual(1, Length.FromMeters(meter.Meters).Meters, Delta);
-            Assert.AreEqual(1, Length.FromDecimeters(meter.Decimeters).Meters, Delta);
-            Assert.AreEqual(1, Length.FromCentimeters(meter.Centimeters).Meters, Delta);
-            Assert.AreEqual(1, Length.FromMillimeters(meter.Millimeters).Meters, Delta);
-            Assert.AreEqual(1, Length.FromMicrometers(meter.Micrometers).Meters, Delta);
-            Assert.AreEqual(1, Length.FromNanometers(meter.Nanometers).Meters, Delta);
-
-            Assert.AreEqual(1, Length.FromMiles(meter.Miles).Meters, Delta);
-            Assert.AreEqual(1, Length.FromYards(meter.Yards).Meters, Delta);
-            Assert.AreEqual(1, Length.FromFeet(meter.Feet).Meters, Delta);
-            Assert.AreEqual(1, Length.FromInches(meter.Inches).Meters, Delta);
-            Assert.AreEqual(1, Length.FromMicroinches(meter.Microinches).Meters, Delta);
-            Assert.AreEqual(1, Length.FromMils(meter.Mils).Meters, Delta);
+            get { return 100; }
         }
 
-        [Test]
-        public void ArithmeticOperators()
+        protected override double DecimetersInOneMeter
         {
-            Length v = Length.FromMeters(1);
-            Assert.AreEqual(-1, -v.Meters, Delta);
-            Assert.AreEqual(2, (Length.FromMeters(3)-v).Meters, Delta);
-            Assert.AreEqual(2, (v + v).Meters, Delta);
-            Assert.AreEqual(10, (v*10).Meters, Delta);
-            Assert.AreEqual(10, (10*v).Meters, Delta);
-            Assert.AreEqual(2, (Length.FromMeters(10)/5).Meters, Delta);
-            Assert.AreEqual(2, Length.FromMeters(10)/Length.FromMeters(5), Delta);
+            get { return 10; }
         }
 
-        [Test]
-        public void ComparisonOperators()
+        protected override double FeetInOneMeter
         {
-            Length oneMeter = Length.FromMeters(1);
-            Length twoMeters = Length.FromMeters(2);
-
-            Assert.True(oneMeter < twoMeters);
-            Assert.True(oneMeter <= twoMeters);
-            Assert.True(twoMeters > oneMeter);
-            Assert.True(twoMeters >= oneMeter);
-
-            Assert.False(oneMeter > twoMeters);
-            Assert.False(oneMeter >= twoMeters);
-            Assert.False(twoMeters < oneMeter);
-            Assert.False(twoMeters <= oneMeter);
+            get { return 3.28084; }
         }
 
-        [Test]
-        public void CompareToIsImplemented()
+        protected override double InchesInOneMeter
         {
-            Length meter = Length.FromMeters(1);
-            Assert.AreEqual(0, meter.CompareTo(meter));
-            Assert.Greater(meter.CompareTo(Length.Zero), 0);
-            Assert.Less(Length.Zero.CompareTo(meter), 0);
+                get { return 39.37007874; }
         }
 
-        [Test]
-        [ExpectedException(typeof(ArgumentException))]
-        public void CompareToThrowsOnTypeMismatch()
+        protected override double KilometersInOneMeter
         {
-            Length meter = Length.FromMeters(1);
-// ReSharper disable once ReturnValueOfPureMethodIsNotUsed
-            meter.CompareTo(new object());
+            get { return 1E-3; }
         }
 
-        [Test]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void CompareToThrowsOnNull()
-        { 
-            Length meter = Length.FromMeters(1);
-// ReSharper disable once ReturnValueOfPureMethodIsNotUsed
-            meter.CompareTo(null);
+        protected override double MetersInOneMeter
+        {
+            get { return 1; }
         }
 
-
-        [Test]
-        public void EqualityOperators()
+        protected override double MicroinchesInOneMeter
         {
-            Length a = Length.FromMeters(1);
-            Length b = Length.FromMeters(2);
-
-// ReSharper disable EqualExpressionComparison
-            Assert.True(a == a); 
-            Assert.True(a != b);
-
-            Assert.False(a == b);
-            Assert.False(a != a);
-// ReSharper restore EqualExpressionComparison
+            get { return 39370078.74015748; }
         }
 
-        [Test]
-        public void EqualsIsImplemented()
+        protected override double MicrometersInOneMeter
         {
-            Length v = Length.FromMeters(1);
-            Assert.IsTrue(v.Equals(Length.FromMeters(1)));
-            Assert.IsFalse(v.Equals(Length.Zero));
+            get { return 1E6; }
         }
 
-        [Test]
-        public void EqualsReturnsFalseOnTypeMismatch()
+        protected override double MilsInOneMeter
         {
-            Length meter = Length.FromMeters(1);
-            Assert.IsFalse(meter.Equals(new object()));
+            get { return 39370.07874015; }
         }
 
-        [Test]
-        public void EqualsReturnsFalseOnNull()
+        protected override double MilesInOneMeter
         {
-            Length meter = Length.FromMeters(1);
-            Assert.IsFalse(meter.Equals(null));
+            get { return 0.000621371; }
+        }
+
+        protected override double MillimetersInOneMeter
+        {
+            get { return 1E3; }
+        }
+
+        protected override double NanometersInOneMeter
+        {
+            get { return 1E9; }
+        }
+
+        protected override double YardsInOneMeter
+        {
+            get { return 1.09361; }
         }
     }
 }
