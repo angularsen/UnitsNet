62ba6c0c832bc11937089b055bd613f5011407b3
Andreas Larsen <anjdreas@gmail.com>
2013-11-16 22:55:49 +0100

    Reimplemented AngleTests using base class. Gradians test is failing, most likely due to bug in implementation.

--- Tests/AngleTests.cs
+++ Tests/AngleTests.cs
@@ -7,128 +7,26 @@ using NUnit.Framework;
 namespace UnitsNet.Tests.net35
 {
     [TestFixture]
-    public class AngleTests
+    public class AngleTests : AngleTestsBase
     {
-        private const double Delta = 1E-5;
-        private readonly Angle _degrees90 = Angle.FromDegrees(90);
-
-        [Test]
-        public void DegreesToAngleUnits()
+        protected override double Delta
         {
-            Assert.AreEqual(90, _degrees90.Degrees, Delta);
-            Assert.AreEqual(Math.PI / 2, _degrees90.Radians, Delta);
-            Assert.AreEqual(100, _degrees90.Gradians, Delta);
+            get { return 1E-5; }
         }
 
-        [Test]
-        public void AngleUnitsRoundTrip()
+        protected override double DegreesInOneDegree
         {
-            Assert.AreEqual(90, Angle.FromDegrees(_degrees90.Degrees).Degrees, Delta);
-            Assert.AreEqual(90, Angle.FromRadians(_degrees90.Radians).Degrees, Delta);
-            Assert.AreEqual(90, Angle.FromGradians(_degrees90.Gradians).Degrees, Delta);
+            get { return 1; }
         }
 
-        [Test]
-        public void ArithmeticOperatorsRoundtrip()
+        protected override double GradiansInOneDegree
         {
-            Angle a = Angle.FromDegrees(90);
-            Assert.AreEqual(-90, -a.Degrees, Delta);
-            Assert.AreEqual(180, (Angle.FromDegrees(270)-a).Degrees, Delta);
-            Assert.AreEqual(180, (a + a).Degrees, Delta);
-            Assert.AreEqual(900, (a * 10).Degrees, Delta);
-            Assert.AreEqual(900, (10 * a).Degrees, Delta);
-            Assert.AreEqual(18.0, (Angle.FromDegrees(90) / 5).Degrees, Delta);
-            Assert.AreEqual(18.0, Angle.FromDegrees(90) / Angle.FromDegrees(5), Delta);
+            get { return 400/360.0; }
         }
 
-        [Test]
-        public void ComparisonOperators()
+        protected override double RadiansInOneDegree
         {
-            Angle degrees90 = Angle.FromDegrees(90);
-            Angle degrees180 = Angle.FromDegrees(180);
-
-            Assert.True(degrees90 < degrees180);
-            Assert.True(degrees90 <= degrees180);
-            Assert.True(degrees180 > degrees90);
-            Assert.True(degrees180 >= degrees90);
-
-// ReSharper disable EqualExpressionComparison
-            Assert.True(degrees90 <= degrees90);
-            Assert.True(degrees90 >= degrees90);
-// ReSharper restore EqualExpressionComparison
-
-            Assert.False(degrees90 > degrees180);
-            Assert.False(degrees90 >= degrees180);
-            Assert.False(degrees180 < degrees90);
-            Assert.False(degrees180 <= degrees90);
-        }
-
-        [Test]
-        public void CompareToIsImplemented()
-        {
-            Angle degree = Angle.FromDegrees(1);
-
-            Assert.AreEqual(0, degree.CompareTo(degree));
-            Assert.Greater(degree.CompareTo(Angle.Zero), 0);
-            Assert.Less(Angle.Zero.CompareTo(degree), 0);
-        }
-
-        [Test]
-        [ExpectedException(typeof(ArgumentException))]
-        public void CompareToThrowsOnTypeMismatch()
-        {
-            Angle angle = Angle.FromDegrees(90);
-
-// ReSharper disable once ReturnValueOfPureMethodIsNotUsed
-            angle.CompareTo(new object());
-        }
-
-        [Test]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void CompareToThrowsOnNull()
-        {
-            Angle angle = Angle.FromDegrees(90);
-
-// ReSharper disable once ReturnValueOfPureMethodIsNotUsed
-            angle.CompareTo(null);
-        }
-
-
-        [Test]
-        public void EqualityOperators()
-        {
-            Angle a = Angle.FromDegrees(90);
-            Angle b = Angle.FromDegrees(180);
-
-// ReSharper disable EqualExpressionComparison
-            Assert.True(a == a); 
-            Assert.True(a != b);
-
-            Assert.False(a == b);
-            Assert.False(a != a);
-// ReSharper restore EqualExpressionComparison
-        }
-
-        [Test]
-        public void EqualsIsImplemented()
-        {
-            Angle a = Angle.FromDegrees(90);
-            Assert.IsTrue(a.Equals(Angle.FromDegrees(90)));
-            Assert.IsFalse(a.Equals(Angle.Zero));
-        }
-
-        [Test]
-        public void EqualsReturnsFalseOnTypeMismatch()
-        {
-            Angle angle = Angle.FromDegrees(0);
-            Assert.IsFalse(angle.Equals(new object()));
-        }
-
-        [Test]
-        public void EqualsReturnsFalseOnNull()
-        {
-            Angle angle = Angle.FromDegrees(0);
-            Assert.IsFalse(angle.Equals(null));
+            get { return Math.PI/2/90; }
         }
     }
 }
