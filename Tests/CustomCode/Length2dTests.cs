13ecde07375b4afe0f61221d1e396cacc35873d1
Andreas Gullberg Larsen <anjdreas@gmail.com>
2014-01-04 22:53:59 +0100

    Clean up code in Tests/CustomCode/.
    * Run R# full clean up.
    * Add missing file headers.

--- Tests/CustomCode/Length2dTests.cs
+++ Tests/CustomCode/Length2dTests.cs
@@ -1,5 +1,23 @@
-﻿// Disable build warning CS1718: Comparison made to same variable; did you mean to compare something else?
-#pragma warning disable 1718
+﻿// Copyright © 2007 by Initial Force AS.  All rights reserved.
+// https://github.com/InitialForce/UnitsNet
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 using NUnit.Framework;
 
@@ -11,36 +29,33 @@ namespace UnitsNet.Tests.net35.CustomCode
     {
         private const double Delta = 1E-5;
 
-        [Test]
-        public void MetersToDistanceUnits()
-        {
-            Length2d meter = Length2d.FromMeters(1,1);
-
-            AssertAreEqual(new Vector2(1), meter.Meters, Delta);
-            AssertAreEqual(new Vector2(1E2), meter.Centimeters, Delta);
-            AssertAreEqual(new Vector2(1E3), meter.Millimeters, Delta);
-            AssertAreEqual(new Vector2(1E-3), meter.Kilometers, Delta);
-            AssertAreEqual(new Vector2(1), meter.Meters, Delta);
-            AssertAreEqual(new Vector2(1E1), meter.Decimeters, Delta);
-            AssertAreEqual(new Vector2(1E2), meter.Centimeters, Delta);
-            AssertAreEqual(new Vector2(1E3), meter.Millimeters, Delta);
-            AssertAreEqual(new Vector2(1E6), meter.Micrometers, Delta); 
-            AssertAreEqual(new Vector2(1E9), meter.Nanometers, Delta);
-
-            Assert.AreEqual(0.000621371, meter.Miles.X, Delta);
-            Assert.AreEqual(0.000621371, meter.Miles.Y, Delta);
-            Assert.AreEqual(1.09361, meter.Yards.X, Delta);
-            Assert.AreEqual(1.09361, meter.Yards.Y, Delta);
-            Assert.AreEqual(3.28084, meter.Feet.X, Delta);
-            Assert.AreEqual(3.28084, meter.Feet.Y, Delta);
-            Assert.AreEqual(39.37007874, meter.Inches.X, Delta);
-            Assert.AreEqual(39.37007874, meter.Inches.Y, Delta);
-        }
-
         private void AssertAreEqual(Vector2 expected, Vector2 actual, double delta)
-        { 
+        {
             Assert.AreEqual(expected.X, actual.X, Delta);
             Assert.AreEqual(expected.Y, actual.Y, Delta);
+        }
+
+
+        [Test]
+        public void ArithmeticOperators()
+        {
+            Length2d v = Length2d.FromMeters(1, 2);
+            Assert.AreEqual(-1, -v.Meters.X, Delta);
+            Assert.AreEqual(-2, -v.Meters.Y, Delta);
+            Assert.AreEqual(2, (Length2d.FromMeters(3, 3) - v).Meters.X, Delta);
+            Assert.AreEqual(1, (Length2d.FromMeters(3, 3) - v).Meters.Y, Delta);
+            Assert.AreEqual(2, (v + v).Meters.X, Delta);
+            Assert.AreEqual(4, (v + v).Meters.Y, Delta);
+            Assert.AreEqual(10, (v*10).Meters.X, Delta);
+            Assert.AreEqual(20, (v*10).Meters.Y, Delta);
+            Assert.AreEqual(10, (10*v).Meters.X, Delta);
+            Assert.AreEqual(20, (10*v).Meters.Y, Delta);
+            Assert.AreEqual(2, (Length2d.FromMeters(2, 2)*v).Meters.X, Delta);
+            Assert.AreEqual(4, (Length2d.FromMeters(2, 2)*v).Meters.Y, Delta);
+            Assert.AreEqual(2, (Length2d.FromMeters(10, 20)/5).Meters.X, Delta);
+            Assert.AreEqual(4, (Length2d.FromMeters(10, 20)/5).Meters.Y, Delta);
+            Assert.AreEqual(2, (Length2d.FromMeters(10, 20)/Length2d.FromMeters(5, 5)).X, Delta);
+            Assert.AreEqual(4, (Length2d.FromMeters(10, 20)/Length2d.FromMeters(5, 5)).Y, Delta);
         }
 
         [Test]
@@ -58,43 +73,13 @@ namespace UnitsNet.Tests.net35.CustomCode
         }
 
         [Test]
-        public void VectorLength()
-        {
-            var v = new Length2d(3, 4);
-            Assert.AreEqual(5, v.Length.Meters);
-        }
-
-
-        [Test]
-        public void ArithmeticOperators()
-        {
-            Length2d v = Length2d.FromMeters(1, 2);
-            Assert.AreEqual(-1, -v.Meters.X, Delta);
-            Assert.AreEqual(-2, -v.Meters.Y, Delta);
-            Assert.AreEqual(2, (Length2d.FromMeters(3, 3)-v).Meters.X, Delta);
-            Assert.AreEqual(1, (Length2d.FromMeters(3, 3)-v).Meters.Y, Delta);
-            Assert.AreEqual(2, (v + v).Meters.X, Delta);
-            Assert.AreEqual(4, (v + v).Meters.Y, Delta);
-            Assert.AreEqual(10, (v*10).Meters.X, Delta);
-            Assert.AreEqual(20, (v*10).Meters.Y, Delta);
-            Assert.AreEqual(10, (10*v).Meters.X, Delta);
-            Assert.AreEqual(20, (10*v).Meters.Y, Delta);
-            Assert.AreEqual(2, (Length2d.FromMeters(2, 2)*v).Meters.X, Delta);
-            Assert.AreEqual(4, (Length2d.FromMeters(2, 2)*v).Meters.Y, Delta);
-            Assert.AreEqual(2, (Length2d.FromMeters(10, 20)/5).Meters.X, Delta);
-            Assert.AreEqual(4, (Length2d.FromMeters(10, 20)/5).Meters.Y, Delta);
-            Assert.AreEqual(2, (Length2d.FromMeters(10, 20)/Length2d.FromMeters(5, 5)).X, Delta);
-            Assert.AreEqual(4, (Length2d.FromMeters(10, 20)/Length2d.FromMeters(5, 5)).Y, Delta);
-        }
-
-        [Test]
         public void EqualityOperators()
         {
             Length2d a = Length2d.FromMeters(1, 2);
             Length2d b = Length2d.FromMeters(2, 1);
 
 // ReSharper disable EqualExpressionComparison
-            Assert.True(a == a); 
+            Assert.True(a == a);
             Assert.True(a != b);
 
             Assert.False(a == b);
@@ -111,6 +96,13 @@ namespace UnitsNet.Tests.net35.CustomCode
         }
 
         [Test]
+        public void EqualsReturnsFalseOnNull()
+        {
+            Length2d newton = Length2d.FromMeters(1, 2);
+            Assert.IsFalse(newton.Equals(null));
+        }
+
+        [Test]
         public void EqualsReturnsFalseOnTypeMismatch()
         {
             Length2d newton = Length2d.FromMeters(1, 2);
@@ -118,10 +110,36 @@ namespace UnitsNet.Tests.net35.CustomCode
         }
 
         [Test]
-        public void EqualsReturnsFalseOnNull()
+        public void MetersToDistanceUnits()
         {
-            Length2d newton = Length2d.FromMeters(1, 2);
-            Assert.IsFalse(newton.Equals(null));
+            Length2d meter = Length2d.FromMeters(1, 1);
+
+            AssertAreEqual(new Vector2(1), meter.Meters, Delta);
+            AssertAreEqual(new Vector2(1E2), meter.Centimeters, Delta);
+            AssertAreEqual(new Vector2(1E3), meter.Millimeters, Delta);
+            AssertAreEqual(new Vector2(1E-3), meter.Kilometers, Delta);
+            AssertAreEqual(new Vector2(1), meter.Meters, Delta);
+            AssertAreEqual(new Vector2(1E1), meter.Decimeters, Delta);
+            AssertAreEqual(new Vector2(1E2), meter.Centimeters, Delta);
+            AssertAreEqual(new Vector2(1E3), meter.Millimeters, Delta);
+            AssertAreEqual(new Vector2(1E6), meter.Micrometers, Delta);
+            AssertAreEqual(new Vector2(1E9), meter.Nanometers, Delta);
+
+            Assert.AreEqual(0.000621371, meter.Miles.X, Delta);
+            Assert.AreEqual(0.000621371, meter.Miles.Y, Delta);
+            Assert.AreEqual(1.09361, meter.Yards.X, Delta);
+            Assert.AreEqual(1.09361, meter.Yards.Y, Delta);
+            Assert.AreEqual(3.28084, meter.Feet.X, Delta);
+            Assert.AreEqual(3.28084, meter.Feet.Y, Delta);
+            Assert.AreEqual(39.37007874, meter.Inches.X, Delta);
+            Assert.AreEqual(39.37007874, meter.Inches.Y, Delta);
+        }
+
+        [Test]
+        public void VectorLength()
+        {
+            var v = new Length2d(3, 4);
+            Assert.AreEqual(5, v.Length.Meters);
         }
     }
 }
