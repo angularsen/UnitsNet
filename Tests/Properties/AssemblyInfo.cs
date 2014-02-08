0b335e5aaa7c1c0bacd9b2addbae32ec4e12f500
Andreas Gullberg Larsen <anjdreas@gmail.com>
2014-02-09 00:55:54 +0100

    Bump to 2.0.0-beta

--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("1.14.0")]
-[assembly: AssemblyFileVersion("1.14.0")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
