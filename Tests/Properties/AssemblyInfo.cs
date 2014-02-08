606f6a290d82627c18cc55c7d827814a89400f67
Andreas Gullberg Larsen <anjdreas@gmail.com>
2014-02-08 20:11:47 +0100

    Bump to 1.14.0-alpha.

--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("1.12.0")]
-[assembly: AssemblyFileVersion("1.12.0")]
+[assembly: AssemblyVersion("1.14.0")]
+[assembly: AssemblyFileVersion("1.14.0")]
