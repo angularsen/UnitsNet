a331accc453b5505ce324a00b8d5a92dbe5cb85d
Andreas Gullberg Larsen <anjdreas@gmail.com>
2014-09-19 14:45:20 +0200

    Add portable45 project to build script

--- Scripts/build.cmd
+++ Scripts/build.cmd
@@ -1,5 +1,6 @@
 @echo off
 SET ROOT=".."
 %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:Rebuild /p:Configuration=Release %ROOT%\Src\UnitsNet\UnitsNet.net35.csproj
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:Rebuild /p:Configuration=Release %ROOT%\Src\UnitsNet\UnitsNet.pcl.csproj
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:Rebuild /p:Configuration=Release %ROOT%\Src\UnitsNet\UnitsNet.Portable40.csproj
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:Rebuild /p:Configuration=Release %ROOT%\Src\UnitsNet\UnitsNet.Portable45.csproj /p:DefineConstants="PORTABLE45"
 pause
