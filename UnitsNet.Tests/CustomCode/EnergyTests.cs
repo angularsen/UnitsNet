7770b68e2e3a3ac86786b5f45a4344400abc7d11
unknown <eetestlab@L15011.iconfitness.com>
2015-06-10 14:50:49 -0600

    This closes anjdreas/UnitsNet#85 - add more units for energy.

--- UnitsNet.Tests/CustomCode/EnergyTests.cs
+++ UnitsNet.Tests/CustomCode/EnergyTests.cs
@@ -30,5 +30,55 @@ namespace UnitsNet.Tests.CustomCode
         protected override double JoulesInOneJoule { get { return 1; } }
         protected override double KilojoulesInOneJoule { get { return 1E-3; } }
         protected override double MegajoulesInOneJoule { get { return 1E-6; } }
+
+        protected override double BritishThermalUnitsInOneJoule
+        {
+            get { return 1/1055.05585262; }
+        }
+
+        protected override double CaloriesInOneJoule
+        {
+            get { return 1/4.184; }
+        }
+
+        protected override double ElectronVoltsInOneJoule
+        {
+            get { return 1/1.602176565e-19; }
+        }
+
+        protected override double ErgsInOneJoule
+        {
+            get { return 1/1e-7; }
+        }
+
+        protected override double FootPoundsInOneJoule
+        {
+            get { return 1/1.355817948; }
+        }
+
+        protected override double GigawattHoursInOneJoule
+        {
+            get { return 1 / (3600d * 1e9); }
+        }
+
+        protected override double KilocaloriesInOneJoule
+        {
+            get { return 1 / (4.184 * 1000); }
+        }
+
+        protected override double KilowattHoursInOneJoule
+        {
+            get { return 1 / (3600d * 1e3); }
+        }
+
+        protected override double MegawattHoursInOneJoule
+        {
+            get { return 1 / (3600d * 1e6); }
+        }
+
+        protected override double WattHoursInOneJoule
+        {
+            get { return 1 /3600d; }
+        }
     }
 }
--- UnitsNet/GeneratedCode/UnitClasses/Energy.g.cs
+++ UnitsNet/GeneratedCode/UnitClasses/Energy.g.cs
@@ -31,7 +31,7 @@ using UnitsNet.Units;
 namespace UnitsNet
 {
     /// <summary>
-    ///     The joule, symbol J, is a derived unit of energy, work, or amount of heat in the International System of Units. It is equal to the energy transferred (or work done) when applying a force of one newton through a distance of one metre (1 newton metre or N·m), or in passing an electric current of one ampere through a resistance of one ohm for one second
+    ///     The joule, symbol J, is a derived unit of energy, work, or amount of heat in the International System of Units. It is equal to the energy transferred (or work done) when applying a force of one newton through a distance of one metre (1 newton metre or N·m), or in passing an electric current of one ampere through a resistance of one ohm for one second. Many other units of energy are included. Please do not confuse this definition of the calorie with the one colloquially used by the food industry, the large calorie, which is equivalent to 1 kcal. Thermochemical definition of the calorie is used. For BTU, the IT definition is used.
     /// </summary>
     // ReSharper disable once PartialTypeWithSinglePart
     public partial struct Energy : IComparable, IComparable<Energy>
@@ -49,11 +49,67 @@ namespace UnitsNet
         #region Properties
 
         /// <summary>
+        ///     Get Energy in BritishThermalUnits.
+        /// </summary>
+        public double BritishThermalUnits
+        {
+            get { return _joules/1055.05585262; }
+        }
+
+        /// <summary>
+        ///     Get Energy in Calories.
+        /// </summary>
+        public double Calories
+        {
+            get { return _joules/4.184; }
+        }
+
+        /// <summary>
+        ///     Get Energy in ElectronVolts.
+        /// </summary>
+        public double ElectronVolts
+        {
+            get { return _joules/1.602176565e-19; }
+        }
+
+        /// <summary>
+        ///     Get Energy in Ergs.
+        /// </summary>
+        public double Ergs
+        {
+            get { return _joules/1e-7; }
+        }
+
+        /// <summary>
+        ///     Get Energy in FootPounds.
+        /// </summary>
+        public double FootPounds
+        {
+            get { return _joules/1.355817948; }
+        }
+
+        /// <summary>
+        ///     Get Energy in GigawattHours.
+        /// </summary>
+        public double GigawattHours
+        {
+            get { return (_joules/3600d) / 1e9d; }
+        }
+
+        /// <summary>
         ///     Get Energy in Joules.
         /// </summary>
         public double Joules
         {
             get { return _joules; }
+        }
+
+        /// <summary>
+        ///     Get Energy in Kilocalories.
+        /// </summary>
+        public double Kilocalories
+        {
+            get { return (_joules/4.184) / 1e3d; }
         }
 
         /// <summary>
@@ -65,11 +121,35 @@ namespace UnitsNet
         }
 
         /// <summary>
+        ///     Get Energy in KilowattHours.
+        /// </summary>
+        public double KilowattHours
+        {
+            get { return (_joules/3600d) / 1e3d; }
+        }
+
+        /// <summary>
         ///     Get Energy in Megajoules.
         /// </summary>
         public double Megajoules
         {
             get { return (_joules) / 1e6d; }
+        }
+
+        /// <summary>
+        ///     Get Energy in MegawattHours.
+        /// </summary>
+        public double MegawattHours
+        {
+            get { return (_joules/3600d) / 1e6d; }
+        }
+
+        /// <summary>
+        ///     Get Energy in WattHours.
+        /// </summary>
+        public double WattHours
+        {
+            get { return _joules/3600d; }
         }
 
         #endregion
@@ -82,11 +162,67 @@ namespace UnitsNet
         }
 
         /// <summary>
+        ///     Get Energy from BritishThermalUnits.
+        /// </summary>
+        public static Energy FromBritishThermalUnits(double britishthermalunits)
+        {
+            return new Energy(britishthermalunits*1055.05585262);
+        }
+
+        /// <summary>
+        ///     Get Energy from Calories.
+        /// </summary>
+        public static Energy FromCalories(double calories)
+        {
+            return new Energy(calories*4.184);
+        }
+
+        /// <summary>
+        ///     Get Energy from ElectronVolts.
+        /// </summary>
+        public static Energy FromElectronVolts(double electronvolts)
+        {
+            return new Energy(electronvolts*1.602176565e-19);
+        }
+
+        /// <summary>
+        ///     Get Energy from Ergs.
+        /// </summary>
+        public static Energy FromErgs(double ergs)
+        {
+            return new Energy(ergs*1e-7);
+        }
+
+        /// <summary>
+        ///     Get Energy from FootPounds.
+        /// </summary>
+        public static Energy FromFootPounds(double footpounds)
+        {
+            return new Energy(footpounds*1.355817948);
+        }
+
+        /// <summary>
+        ///     Get Energy from GigawattHours.
+        /// </summary>
+        public static Energy FromGigawattHours(double gigawatthours)
+        {
+            return new Energy((gigawatthours*3600d) * 1e9d);
+        }
+
+        /// <summary>
         ///     Get Energy from Joules.
         /// </summary>
         public static Energy FromJoules(double joules)
         {
             return new Energy(joules);
+        }
+
+        /// <summary>
+        ///     Get Energy from Kilocalories.
+        /// </summary>
+        public static Energy FromKilocalories(double kilocalories)
+        {
+            return new Energy((kilocalories*4.184) * 1e3d);
         }
 
         /// <summary>
@@ -98,11 +234,35 @@ namespace UnitsNet
         }
 
         /// <summary>
+        ///     Get Energy from KilowattHours.
+        /// </summary>
+        public static Energy FromKilowattHours(double kilowatthours)
+        {
+            return new Energy((kilowatthours*3600d) * 1e3d);
+        }
+
+        /// <summary>
         ///     Get Energy from Megajoules.
         /// </summary>
         public static Energy FromMegajoules(double megajoules)
         {
             return new Energy((megajoules) * 1e6d);
+        }
+
+        /// <summary>
+        ///     Get Energy from MegawattHours.
+        /// </summary>
+        public static Energy FromMegawattHours(double megawatthours)
+        {
+            return new Energy((megawatthours*3600d) * 1e6d);
+        }
+
+        /// <summary>
+        ///     Get Energy from WattHours.
+        /// </summary>
+        public static Energy FromWattHours(double watthours)
+        {
+            return new Energy(watthours*3600d);
         }
 
 
@@ -116,12 +276,32 @@ namespace UnitsNet
         {
             switch (fromUnit)
             {
+                case EnergyUnit.BritishThermalUnit:
+                    return FromBritishThermalUnits(value);
+                case EnergyUnit.Calorie:
+                    return FromCalories(value);
+                case EnergyUnit.ElectronVolt:
+                    return FromElectronVolts(value);
+                case EnergyUnit.Erg:
+                    return FromErgs(value);
+                case EnergyUnit.FootPound:
+                    return FromFootPounds(value);
+                case EnergyUnit.GigawattHour:
+                    return FromGigawattHours(value);
                 case EnergyUnit.Joule:
                     return FromJoules(value);
+                case EnergyUnit.Kilocalorie:
+                    return FromKilocalories(value);
                 case EnergyUnit.Kilojoule:
                     return FromKilojoules(value);
+                case EnergyUnit.KilowattHour:
+                    return FromKilowattHours(value);
                 case EnergyUnit.Megajoule:
                     return FromMegajoules(value);
+                case EnergyUnit.MegawattHour:
+                    return FromMegawattHours(value);
+                case EnergyUnit.WattHour:
+                    return FromWattHours(value);
 
                 default:
                     throw new NotImplementedException("fromUnit: " + fromUnit);
@@ -255,12 +435,32 @@ namespace UnitsNet
         {
             switch (unit)
             {
+                case EnergyUnit.BritishThermalUnit:
+                    return BritishThermalUnits;
+                case EnergyUnit.Calorie:
+                    return Calories;
+                case EnergyUnit.ElectronVolt:
+                    return ElectronVolts;
+                case EnergyUnit.Erg:
+                    return Ergs;
+                case EnergyUnit.FootPound:
+                    return FootPounds;
+                case EnergyUnit.GigawattHour:
+                    return GigawattHours;
                 case EnergyUnit.Joule:
                     return Joules;
+                case EnergyUnit.Kilocalorie:
+                    return Kilocalories;
                 case EnergyUnit.Kilojoule:
                     return Kilojoules;
+                case EnergyUnit.KilowattHour:
+                    return KilowattHours;
                 case EnergyUnit.Megajoule:
                     return Megajoules;
+                case EnergyUnit.MegawattHour:
+                    return MegawattHours;
+                case EnergyUnit.WattHour:
+                    return WattHours;
 
                 default:
                     throw new NotImplementedException("unit: " + unit);
