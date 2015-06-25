0c36667a2cdc0df4ac761e8af5631984e71a265f
Andreas Gullberg Larsen <anjdreas@gmail.com>
2015-06-25 22:48:31 +0200

    Build: Read nuget API key from file
    After moving owner of repo, the build server was no longer authorized to push
    nugets.
    
    Update publish script to read API key from file
    Ignore Build/nuget-apikey.txt
    Add placeholder file with instructions

--- Build/nuget-publish.ps1
+++ Build/nuget-publish.ps1
@@ -1,9 +1,13 @@
 $root="$PSScriptRoot\.."
 $nugetDir="$root\Artifacts\NuGet\"
+$apiKeyFile="nuget-apikey.txt"
+
+$apiKey = Get-Content $apiKeyFile
+Write-Host "Using API key: $apiKey"
 
 $list = gci -Path $nugetDir -Filter UnitsNet.*.nupkg | where-object {$_.Name -NotMatch "\.symbols\."}
 foreach ($file in $list)
 {
-  write-host "Publishing nuget: $($file.Name)"
-  Invoke-Expression "$root\Tools\nuget.exe push $($file.FullName)"
+  Write-Host "Publishing nuget: $($file.Name)"
+  Invoke-Expression "$root\Tools\nuget.exe push -ApiKey $apiKey $($file.FullName)"
 }
