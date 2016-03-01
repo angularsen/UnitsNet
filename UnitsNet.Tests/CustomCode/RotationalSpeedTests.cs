e6ba20a52b8475d37ed07a2c5785b998e9359b70
tongbong <francois.tanguy@agileo-automation.com>
2016-03-01 08:34:30 +0100

    Use hardcoded values instead of formulas

--- UnitsNet.Tests/CustomCode/RotationalSpeedTests.cs
+++ UnitsNet.Tests/CustomCode/RotationalSpeedTests.cs
@@ -38,17 +38,17 @@ namespace UnitsNet.Tests.CustomCode
 
         protected override double NanoradiansPerSecondInOneRadianPerSecond => 1E9;
 
-        protected override double RevolutionsPerMinuteInOneRadianPerSecond => 60*1/(2*Math.PI);
+        protected override double RevolutionsPerMinuteInOneRadianPerSecond => 9.54929659;
 
-        protected override double RevolutionsPerSecondInOneRadianPerSecond => 1/(2*Math.PI);
+        protected override double RevolutionsPerSecondInOneRadianPerSecond => 0.15915494;
 
-        protected override double DegreesPerSecondInOneRadianPerSecond => (180 / Math.PI) * 1;
+        protected override double DegreesPerSecondInOneRadianPerSecond => 57.29577951308;
 
-        protected override double MillidegreesPerSecondInOneRadianPerSecond => ((180 / Math.PI) * 1)* 1E3;
+        protected override double MillidegreesPerSecondInOneRadianPerSecond => 57295.77951308;
 
-        protected override double MicrodegreesPerSecondInOneRadianPerSecond => ((180 / Math.PI) * 1) * 1E6;
+        protected override double MicrodegreesPerSecondInOneRadianPerSecond => 57295779.51308232;
 
-        protected override double NanodegreesPerSecondInOneRadianPerSecond => ((180 / Math.PI) * 1) * 1E9;
+        protected override double NanodegreesPerSecondInOneRadianPerSecond => 57295779513.08232087;
 
 
         [Test]
