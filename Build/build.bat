4d2530f909a4373ef48e57a03b7f30c9d83b5a27
Andreas Gullberg Larsen <anjdreas@gmail.com>
2016-03-02 09:50:06 +0100

    build.bat: Add step to regenerate source code
    Ensure we are running the latest changes.
    Typically happens when merging multiple PRs at the same time.

--- Build/build.bat
+++ Build/build.bat
@@ -3,6 +3,7 @@ SET ROOT=%~dp0..
 rmdir /Q /S %ROOT%\Artifacts
 
 call powershell -NoProfile %ROOT%\Build\UpdateAssemblyInfo.ps1
+call powershell -ExecutionPolicy Bypass -NoProfile -File %ROOT%\UnitsNet\Scripts\GenerateUnits.ps1
 call %ROOT%\Build\nuget-restore.bat
 call %ROOT%\Build\build-src-release.bat
 call %ROOT%\Build\build-tests.bat
