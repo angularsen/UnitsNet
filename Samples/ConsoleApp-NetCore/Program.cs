085bc4f99f78de94d41e94c43c140f5a71b8abce
Andreas Gullberg Larsen <anjdreas@gmail.com>
2016-10-22 02:01:57 +0200

    samples: Add solution with .NET Core console app
    That references core nuget, to test netstandard10 behavior.

---
+++ Samples/ConsoleApp-NetCore/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnitsNet;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Length: " + Length.FromMeters(1));
+            Console.ReadKey(true);
+        }
+    }
+}
