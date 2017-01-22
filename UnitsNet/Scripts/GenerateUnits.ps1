2825868cd026de859880b446b17051e80437354e
Andreas Gullberg Larsen <anjdreas@gmail.com>
2017-01-22 22:24:28 +0100

    scripts: Support `InheritUnitsFrom: []` property
    Appends units from one or more unit classes defined in array.
    Note that the `Units: []` property must still exist, even if empty.
    The usecase is to mirror/copy the units of another unit class, such as
    TemperatureDelta inheriting units of Temperature.

--- UnitsNet/Scripts/GenerateUnits.ps1
+++ UnitsNet/Scripts/GenerateUnits.ps1
@@ -8,96 +8,6 @@ function ToCamelCase($str)
     return $str.Substring(0,1).ToLowerInvariant() + $str.Substring(1)
 }
 
-function GetUnits($unitClass)
-{
-    $units = @();
-
-    foreach ($unit in $unitClass.Units)
-    {
-        $units += $unit;
-        $prefixIndex = 0;
-        foreach ($prefix in $unit.Prefixes)
-        {
-            $prefixInfo = switch ($prefix)
-            {
-                "Kilo" { "k", "1e3d"; break; }
-                "Hecto" { "h", "1e2d"; break; }
-                "Deca" { "da", "1e1d"; break; }
-                "Deci" { "d", "1e-1d"; break; }
-                "Centi" { "c", "1e-2d"; break; }
-                "Milli" { "m", "1e-3d"; break; }
-                "Micro" { "μ", "1e-6d"; break; }
-                "Nano" { "n", "1e-9d"; break; }
-
-                # Optimization, move less frequently used prefixes to the end
-                "Pico" { "p", "1e-12d"; break; }
-                "Femto" { "f", "1e-15d"; break; }
-                "Atto" { "a", "1e-18d"; break; }
-                "Zepto" { "z", "1e-21d"; break; }
-                "Yocto" { "y", "1e-24d"; break; }
-
-                "Yotta" { "Y", "1e24d"; break; }
-                "Zetta" { "Z", "1e21d"; break; }
-                "Exa" { "E", "1e18d"; break; }
-                "Peta" { "P", "1e15d"; break; }
-                "Tera" { "T", "1e12d"; break; }
-                "Giga" { "G", "1e9d"; break; }
-                "Mega" { "M", "1e6d"; break; }
-
-                # Binary prefixes
-                "Kibi" { "Ki", "1024d"; break; }
-                "Mebi" { "Mi", "(1024d * 1024)"; break; }
-                "Gibi" { "Gi", "(1024d * 1024 * 1024)"; break; }
-                "Tebi" { "Ti", "(1024d * 1024 * 1024 * 1024)"; break; }
-                "Pebi" { "Pi", "(1024d * 1024 * 1024 * 1024 * 1024)"; break; }
-                "Exbi" { "Ei", "(1024d * 1024 * 1024 * 1024 * 1024 * 1024)"; break; }
-            }
-
-            $prefixAbbreviation = $prefixInfo[0];
-            $prefixFactor = $prefixInfo[1];
-
-            $u = New-Object PsObject -Property @{
-                SingularName=$prefix + $(ToCamelCase $unit.SingularName);
-                PluralName=$prefix + $(ToCamelCase $unit.PluralName);
-                FromUnitToBaseFunc="("+$unit.FromUnitToBaseFunc+") * $prefixFactor";
-                FromBaseToUnitFunc="("+$unit.FromBaseToUnitFunc+") / $prefixFactor";
-                Localization=$unit.Localization | % {
-                    $abbrev = $prefixAbbreviation + $_.Abbreviations[0];
-                    if ($_.AbbreviationsWithPrefixes) {
-                        $abbrev = $_.AbbreviationsWithPrefixes[$prefixIndex++];
-                    }
-
-                New-Object PsObject -Property @{
-                    Culture=$_.Culture;
-                    Abbreviations= $abbrev;
-                }};
-            }
-
-            $units += $u;
-        }
-    }
-
-    foreach ($u in $units) {
-        # Use decimal for internal calculations if base type is not double, such as for long or int.
-        if ($unitClass.BaseType -ne "double") {
-            $u.FromUnitToBaseFunc = $u.FromUnitToBaseFunc -replace "m", "d";
-            $u.FromBaseToUnitFunc = $u.FromBaseToUnitFunc -replace "d", "m";
-        }
-
-        # Convert to/from double for other base types
-        if ($unitClass.BaseType -eq "decimal") {
-            $u.FromUnitToBaseFunc = "Convert.ToDecimal($($u.FromUnitToBaseFunc))";
-            $u.FromBaseToUnitFunc = "Convert.ToDouble($($u.FromBaseToUnitFunc))";
-        } else {
-            if ($unitClass.BaseType -eq "long") {
-              $u.FromUnitToBaseFunc = "Convert.ToInt64($($u.FromUnitToBaseFunc))";
-              $u.FromBaseToUnitFunc = "Convert.ToDouble($($u.FromBaseToUnitFunc))";
-            }
-        }
-    }
-
-    return $units | sort SingularName;
-}
 
 function GenerateUnitClass($unitClass, $outDir)
 {
@@ -198,6 +108,146 @@ function EnsureDirExists([String] $dirPath) {
     }
 }
 
+function Set-DefaultValues {
+    param ([Parameter(Mandatory = $true, ValueFromPipeline=$true)] $unitClass)
+    PROCESS {
+        if (!$unitClass.BaseType) {
+            $unitClass | Add-Member BaseType "double"
+        }
+        if ($unitClass.GenerateArithmetic -eq $null) {
+            $unitClass | Add-Member GenerateArithmetic $true
+        }
+        # 'Logarithmic' is optional in the .json file and assumed to be false if not specified
+        if (!$unitClass.Logarithmic) {
+            $unitClass | Add-Member Logarithmic $false
+        }
+        elseif (!$unitClass.LogarithmicScalingFactor) {
+            $unitClass | Add-Member LogarithmicScalingFactor 1
+        }
+        return $unitClass
+    }
+}
+
+function Set-ConversionFunctions
+{
+    param ([Parameter(Mandatory = $true, ValueFromPipeline=$true)] $unitClass)
+    PROCESS {
+        foreach ($u in $unitClass.Units) {
+
+            # Use decimal for internal calculations if base type is not double, such as for long or int.
+            if ($unitClass.BaseType -ne "double") {
+                $u.FromUnitToBaseFunc = $u.FromUnitToBaseFunc -replace "m", "d"
+                $u.FromBaseToUnitFunc = $u.FromBaseToUnitFunc -replace "d", "m"
+            }
+
+            # Convert to/from double for other base types
+            if ($unitClass.BaseType -eq "decimal") {
+                $u.FromUnitToBaseFunc = "Convert.ToDecimal($($u.FromUnitToBaseFunc))"
+                $u.FromBaseToUnitFunc = "Convert.ToDouble($($u.FromBaseToUnitFunc))"
+            } else {
+                if ($unitClass.BaseType -eq "long") {
+                  $u.FromUnitToBaseFunc = "Convert.ToInt64($($u.FromUnitToBaseFunc))"
+                  $u.FromBaseToUnitFunc = "Convert.ToDouble($($u.FromBaseToUnitFunc))"
+                }
+            }
+        }
+        return $unitClass
+    }
+}
+
+function Add-PrefixUnits {
+    param ([Parameter(Mandatory = $true, ValueFromPipeline=$true)] $unitClass)
+    PROCESS {
+        $prefixUnits = @()
+
+        foreach ($unit in $unitClass.Units)
+        {
+            $prefixIndex = 0
+            foreach ($prefix in $unit.Prefixes)
+            {
+                $prefixInfo = switch ($prefix)
+                {
+                    "Kilo" { "k", "1e3d"; break; }
+                    "Hecto" { "h", "1e2d"; break; }
+                    "Deca" { "da", "1e1d"; break; }
+                    "Deci" { "d", "1e-1d"; break; }
+                    "Centi" { "c", "1e-2d"; break; }
+                    "Milli" { "m", "1e-3d"; break; }
+                    "Micro" { "μ", "1e-6d"; break; }
+                    "Nano" { "n", "1e-9d"; break; }
+
+                    # Optimization, move less frequently used prefixes to the end
+                    "Pico" { "p", "1e-12d"; break; }
+                    "Femto" { "f", "1e-15d"; break; }
+                    "Atto" { "a", "1e-18d"; break; }
+                    "Zepto" { "z", "1e-21d"; break; }
+                    "Yocto" { "y", "1e-24d"; break; }
+
+                    "Yotta" { "Y", "1e24d"; break; }
+                    "Zetta" { "Z", "1e21d"; break; }
+                    "Exa" { "E", "1e18d"; break; }
+                    "Peta" { "P", "1e15d"; break; }
+                    "Tera" { "T", "1e12d"; break; }
+                    "Giga" { "G", "1e9d"; break; }
+                    "Mega" { "M", "1e6d"; break; }
+
+                    # Binary prefixes
+                    "Kibi" { "Ki", "1024d"; break; }
+                    "Mebi" { "Mi", "(1024d * 1024)"; break; }
+                    "Gibi" { "Gi", "(1024d * 1024 * 1024)"; break; }
+                    "Tebi" { "Ti", "(1024d * 1024 * 1024 * 1024)"; break; }
+                    "Pebi" { "Pi", "(1024d * 1024 * 1024 * 1024 * 1024)"; break; }
+                    "Exbi" { "Ei", "(1024d * 1024 * 1024 * 1024 * 1024 * 1024)"; break; }
+                }
+
+                $prefixAbbreviation = $prefixInfo[0]
+                $prefixFactor = $prefixInfo[1]
+
+                $prefixUnit = New-Object PsObject -Property @{
+                    SingularName=$prefix + $(ToCamelCase $unit.SingularName)
+                    PluralName=$prefix + $(ToCamelCase $unit.PluralName)
+                    FromUnitToBaseFunc="("+$unit.FromUnitToBaseFunc+") * $prefixFactor"
+                    FromBaseToUnitFunc="("+$unit.FromBaseToUnitFunc+") / $prefixFactor"
+                    Localization=$unit.Localization | % {
+                        $abbrev = $prefixAbbreviation + $_.Abbreviations[0]
+                        if ($_.AbbreviationsWithPrefixes) {
+                            $abbrev = $_.AbbreviationsWithPrefixes[$prefixIndex++]
+                        }
+
+                    New-Object PsObject -Property @{
+                        Culture=$_.Culture
+                        Abbreviations= $abbrev
+                    }}
+                }
+
+                # Append prefix unit
+                $prefixUnits += $prefixUnit
+            } # foreach prefixes
+        } # foreach units
+
+    $unitClass.Units += $prefixUnits
+    return $unitClass
+    }
+}
+
+function Set-UnitsOrderedByName {
+    param ([Parameter(Mandatory = $true, ValueFromPipeline=$true)] $unitClass)
+    PROCESS {
+        $unitClass.Units = ($unitClass.Units | sort SingularName)
+        return $unitClass
+    }
+}
+
+function Add-InheritedUnits($unitClass, $unitClasses) {
+
+    foreach ($inheritFromUnitClassName in $unitClass.InheritUnitsFrom) {
+        $inheritFromUnitClass = $unitClasses | Where { $_.Name -eq $inheritFromUnitClassName } | Select -First 1
+        $unitClass.Units += $inheritFromUnitClass.Units
+
+        Write-Host -NoNewline "(inherit $inheritFromUnitClassName) "
+    }
+}
+
 # Load external generator functions with same name as file
 . "$PSScriptRoot/Include-GenerateTemplates.ps1"
 . "$PSScriptRoot/Include-GenerateLogarithmicCode.ps1"
@@ -217,37 +267,28 @@ EnsureDirExists ($numberExtensionsDir = "$PSScriptRoot/../GeneratedCode/Extensio
 EnsureDirExists ($testsCustomCodeDir = "$PSScriptRoot/../../UnitsNet.Tests/CustomCode")
 
 $templatesDir = "$PSScriptRoot/../UnitDefinitions"
-$unitClasses = @();
-
 $pad = 25
-get-childitem -path $templatesDir -filter "*.json" | % {
-    $templateFile = $_.FullName
-    $json = (Get-Content $templateFile | Out-String)
-    $unitClass = $json | ConvertFrom-Json
 
-    # Set default values
-    if (!$unitClass.BaseType) {
-        $unitClass | Add-Member BaseType "double";
-    }
-    # 'Logarithmic' is optional in the .json file and assumed to be false if not specified
-    if (!$unitClass.Logarithmic) {
-        $unitClass | Add-Member Logarithmic "False"
-    }
-    elseif (!$unitClass.LogarithmicScalingFactor) {
-        $unitClass | Add-Member LogarithmicScalingFactor "1"
-    }
+# Parse unit definitions from .json files and populate properties
+$unitClasses = Get-ChildItem -Path $templatesDir -filter "*.json" `
+    | %{(Get-Content $_.FullName | Out-String)} `
+    | ConvertFrom-Json `
+    | Add-PrefixUnits `
+    | Set-DefaultValues `
+    | Set-ConversionFunctions `
+    | Set-UnitsOrderedByName
 
-    # Expand unit prefixes into units
-    $unitClass.Units = GetUnits $unitClass;
-
+foreach ($unitClass in $unitClasses) {
     Write-Host -NoNewline "$($unitClass.Name):".PadRight($pad)
+
+    Add-InheritedUnits $unitClass $unitClasses
+
     GenerateUnitClass $unitClass $unitClassDir
     GenerateUnitEnum $unitClass $unitEnumDir
     GenerateNumberExtensions $unitClass $numberExtensionsDir
     GenerateUnitTestBaseClass $unitClass $testsDir
     GenerateUnitTestClassIfNotExists $unitClass $testsCustomCodeDir
 
-    $unitClasses += $unitClass;
     Write-Host ""
 }
 
