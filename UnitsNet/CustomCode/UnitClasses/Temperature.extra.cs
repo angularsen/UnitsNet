70db889f2baffc896a4f29fb24bf912aa21c289e
Andreas Gullberg Larsen <anjdreas@gmail.com>
2017-01-24 23:20:46 +0100

    Temperature: Add Multiply() and Divide() methods
    Cannot use TemperatureDelta with * and / operators, since different temperature
    units have different zero points, so one must first convert to a unit of
    choice, then perform the arithmetic, then convert back to Temperature.

--- UnitsNet/CustomCode/UnitClasses/Temperature.extra.cs
+++ UnitsNet/CustomCode/UnitClasses/Temperature.extra.cs
@@ -21,51 +21,86 @@
 
 #if !WINDOWS_UWP
 // Operator overloads not supported in Universal Windows Platform (WinRT Components)
-using System;
+using UnitsNet.Units;
 
 namespace UnitsNet
 {
     public partial struct Temperature
     {
+        /// <summary>
+        ///     Add a <see cref="Temperature" /> and a <see cref="TemperatureDelta" />.
+        /// </summary>
+        /// <remarks>Due to temperature units having different scales, the arithmetic must be performed on the same scale.</remarks>
+        /// <returns>The new temperature.</returns>
         public static Temperature operator +(Temperature left, TemperatureDelta right)
         {
             return new Temperature(left.Kelvins + right.KelvinsDelta);
         }
 
+        /// <summary>
+        ///     Add a <see cref="TemperatureDelta" /> and a <see cref="Temperature" />.
+        /// </summary>
+        /// <remarks>Due to temperature units having different scales, the arithmetic must be performed on the same scale.</remarks>
+        /// <returns>The new temperature.</returns>
         public static Temperature operator +(TemperatureDelta left, Temperature right)
         {
             return new Temperature(left.KelvinsDelta + right.Kelvins);
         }
 
+        /// <summary>
+        ///     Subtract a <see cref="Temperature" /> by a <see cref="TemperatureDelta" />.
+        /// </summary>
+        /// <remarks>Due to temperature units having different scales, the arithmetic must be performed on the same scale.</remarks>
+        /// <returns>The new temperature.</returns>
         public static Temperature operator -(Temperature left, TemperatureDelta right)
         {
             return new Temperature(left.Kelvins - right.KelvinsDelta);
         }
 
+        /// <summary>
+        ///     Subtract a <see cref="Temperature" /> by a <see cref="TemperatureDelta" />.
+        /// </summary>
+        /// <remarks>Due to temperature units having different scales, the arithmetic must be performed on the same scale.</remarks>
+        /// <returns>The delta temperature (difference).</returns>
         public static TemperatureDelta operator -(Temperature left, Temperature right)
         {
             return new TemperatureDelta(left.Kelvins - right.Kelvins);
         }
 
-//        public static Temperature operator *(double left, Temperature right)
-//        {
-//            return new Temperature(left * right.Kelvins);
-//        }
-//
-//        public static Temperature operator *(Temperature left, double right)
-//        {
-//            return new Temperature(left.Kelvins * right);
-//        }
-//
-//        public static Temperature operator /(Temperature left, double right)
-//        {
-//            return new Temperature(left.Kelvins / right);
-//        }
-//
-//        public static double operator /(Temperature left, Temperature right)
-//        {
-//            return Convert.ToDouble(left.Kelvins / right.Kelvins);
-//        }
+        /// <summary>
+        ///     Multiply temperature with a <paramref name="factor" /> in a given <paramref name="unit" />.
+        /// </summary>
+        /// <remarks>
+        ///     Due to different temperature units having different zero points, we cannot simply
+        ///     multiply or divide a temperature by a factor. We must first convert to the desired unit, then perform the
+        ///     calculation.
+        /// </remarks>
+        /// <param name="factor">Factor to multiply by.</param>
+        /// <param name="unit">Unit to perform multiplication in.</param>
+        /// <returns>The resulting <see cref="Temperature" />.</returns>
+        public Temperature Multiply(double factor, TemperatureUnit unit)
+        {
+            double resultInUnit = As(unit) * factor;
+            return From(resultInUnit, unit);
+        }
+
+
+        /// <summary>
+        ///     Divide temperature by a <paramref name="divisor" /> in a given <paramref name="unit" />.
+        /// </summary>
+        /// <remarks>
+        ///     Due to different temperature units having different zero points, we cannot simply
+        ///     multiply or divide a temperature by a factor. We must first convert to the desired unit, then perform the
+        ///     calculation.
+        /// </remarks>
+        /// <param name="divisor">Factor to multiply by.</param>
+        /// <param name="unit">Unit to perform multiplication in.</param>
+        /// <returns>The resulting <see cref="Temperature" />.</returns>
+        public Temperature Divide(double divisor, TemperatureUnit unit)
+        {
+            double resultInUnit = As(unit) / divisor;
+            return From(resultInUnit, unit);
+        }
     }
 }
 
