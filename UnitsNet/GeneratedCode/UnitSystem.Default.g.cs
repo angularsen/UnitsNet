646faa597be3160f9a42ad8ea6ca5546c40b4b54
salido-m <maxime.salido@gmail.com>
2017-03-15 10:31:27 +0100

    Adding RatioChangeRate json file in new UnitDefinitions folder

--- UnitsNet/GeneratedCode/UnitSystem.Default.g.cs
+++ UnitsNet/GeneratedCode/UnitSystem.Default.g.cs
@@ -1838,6 +1838,20 @@ namespace UnitsNet
                                 new AbbreviationsForCulture("en-US", "%"),
                             }),
                     }),
+                new UnitLocalization(typeof (RatioChangeRateUnit),
+                    new[]
+                    {
+                        new CulturesForEnumValue((int) RatioChangeRateUnit.DecimalFractionPerSecond,
+                            new[]
+                            {
+                                new AbbreviationsForCulture("en-US", ""),
+                            }),
+                        new CulturesForEnumValue((int) RatioChangeRateUnit.PercentPerSecond,
+                            new[]
+                            {
+                                new AbbreviationsForCulture("en-US", "%/s"),
+                            }),
+                    }),
                 new UnitLocalization(typeof (ReactivePowerUnit),
                     new[]
                     {
