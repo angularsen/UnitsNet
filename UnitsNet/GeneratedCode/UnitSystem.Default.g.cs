139fb65aa2dba22f9cf8c998ee8d789181c8d355
gratestas <stasgrate@gmail.com>
2017-11-24 18:15:49 +0300

    New Quantity: SpecificEntropy (#322)
    implemented new quantity SpecificEntropy

--- UnitsNet/GeneratedCode/UnitSystem.Default.g.cs
+++ UnitsNet/GeneratedCode/UnitSystem.Default.g.cs
@@ -2707,6 +2707,50 @@ namespace UnitsNet
                                 new AbbreviationsForCulture("en-US", "Wh/kg"),
                             }),
                     }),
+                new UnitLocalization(typeof (SpecificEntropyUnit),
+                    new[]
+                    {
+                        new CulturesForEnumValue((int) SpecificEntropyUnit.CaloriePerGramKelvin,
+                            new[]
+                            {
+                                new AbbreviationsForCulture("en-US", "cal/g.K"),
+                            }),
+                        new CulturesForEnumValue((int) SpecificEntropyUnit.JoulePerKilogramDegreeCelsius,
+                            new[]
+                            {
+                                new AbbreviationsForCulture("en-US", "J/kg.C"),
+                            }),
+                        new CulturesForEnumValue((int) SpecificEntropyUnit.JoulePerKilogramKelvin,
+                            new[]
+                            {
+                                new AbbreviationsForCulture("en-US", "J/kg.K"),
+                            }),
+                        new CulturesForEnumValue((int) SpecificEntropyUnit.KilocaloriePerGramKelvin,
+                            new[]
+                            {
+                                new AbbreviationsForCulture("en-US", "kcal/g.K"),
+                            }),
+                        new CulturesForEnumValue((int) SpecificEntropyUnit.KilojoulePerKilogramDegreeCelsius,
+                            new[]
+                            {
+                                new AbbreviationsForCulture("en-US", "kJ/kg.C"),
+                            }),
+                        new CulturesForEnumValue((int) SpecificEntropyUnit.KilojoulePerKilogramKelvin,
+                            new[]
+                            {
+                                new AbbreviationsForCulture("en-US", "kJ/kg.K"),
+                            }),
+                        new CulturesForEnumValue((int) SpecificEntropyUnit.MegajoulePerKilogramDegreeCelsius,
+                            new[]
+                            {
+                                new AbbreviationsForCulture("en-US", "MJ/kg.C"),
+                            }),
+                        new CulturesForEnumValue((int) SpecificEntropyUnit.MegajoulePerKilogramKelvin,
+                            new[]
+                            {
+                                new AbbreviationsForCulture("en-US", "MJ/kg.K"),
+                            }),
+                    }),
                 new UnitLocalization(typeof (SpecificWeightUnit),
                     new[]
                     {
