3137cae68917c2ced98e83fca8e091efaabee743
Tristan Milnthorp <tristan.milnthorp@ansys.com>
2018-06-13 14:36:11 -0400

    Adding base dimensions to units

---
+++ UnitsNet/BaseDimensions.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) 2013 Andreas Gullberg Larsen (andreas.larsen84@gmail.com).
+// https://github.com/angularsen/UnitsNet
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+
+namespace UnitsNet
+{
+    public struct BaseDimensions
+    {
+        public readonly int
+            Length,             // L
+            Mass,               // M
+            Time,               // T
+            Current,            // I
+            Temperature,        // Θ
+            Amount,             // N
+            LuminousIntensity;  // J
+
+        public BaseDimensions( int length, int mass, int time, int current, int temperature, int amount, int luminousIntensity )
+        {
+            Length = length;
+            Mass = mass;
+            Time = time;
+            Current = current;
+            Temperature = temperature;
+            Amount = amount;
+            LuminousIntensity = luminousIntensity;
+        }
+    }
+}
