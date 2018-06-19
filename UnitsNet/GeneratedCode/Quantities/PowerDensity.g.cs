00cc83c22bcfff59571a874f63fe8e01d6d7777e
Tristan Milnthorp <tmilnthorp@gmail.com>
2018-06-19 16:16:07 -0400

    Adding base dimensions for power density

--- UnitsNet/GeneratedCode/Quantities/PowerDensity.g.cs
+++ UnitsNet/GeneratedCode/Quantities/PowerDensity.g.cs
@@ -158,6 +158,16 @@ namespace UnitsNet
         /// </summary>
         public static PowerDensityUnit BaseUnit => PowerDensityUnit.WattPerCubicMeter;
 
+        private static readonly BaseDimensions _baseDimensions = new BaseDimensions(-1, 1, -3, 0, 0, 0, 0);
+
+        /// <summary>
+        ///     The <see cref="BaseDimensions" /> of this quantity.
+        /// </summary>
+        public static BaseDimensions BaseDimensions
+        {
+            get{ return _baseDimensions; }
+        }
+
         /// <summary>
         ///     All units of measurement for the PowerDensity quantity.
         /// </summary>
@@ -2253,7 +2263,7 @@ namespace UnitsNet
         /// <returns>The value in the base unit representation.</returns>
         private double AsBaseUnitWattsPerCubicMeter()
         {
-			if (Unit == PowerDensityUnit.WattPerCubicMeter) { return _value; }
+            if (Unit == PowerDensityUnit.WattPerCubicMeter) { return _value; }
 
             switch (Unit)
             {
@@ -2303,10 +2313,15 @@ namespace UnitsNet
                 case PowerDensityUnit.WattPerLiter: return _value*1.0e3;
                 default:
                     throw new NotImplementedException("Unit not implemented: " + Unit);
-			}
-		}
+            }
+        }
 
-		/// <summary>Convenience method for working with internal numeric type.</summary>
+        /// <summary>Convenience method for working with internal numeric type.</summary>
         private double AsBaseNumericType(PowerDensityUnit unit) => Convert.ToDouble(As(unit));
-	}
+
+        /// <summary>
+        ///     The <see cref="BaseDimensions" /> of this quantity.
+        /// </summary>
+        public BaseDimensions Dimensions => PowerDensity.BaseDimensions;
+    }
 }
