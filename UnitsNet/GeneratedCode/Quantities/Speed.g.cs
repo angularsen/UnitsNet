5c5db3c402315b5179146dc09fa6db71c592cc58
Tristan Milnthorp <tristan.milnthorp@ansys.com>
2018-06-25 17:48:50 -0400

    Updating documentation to be more clear and provide examples for quantity types

--- UnitsNet/GeneratedCode/Quantities/Speed.g.cs
+++ UnitsNet/GeneratedCode/Quantities/Speed.g.cs
@@ -1423,12 +1423,30 @@ namespace UnitsNet
         ///     </para>
         ///     <para>
         ///     Relative tolerance is defined as the maximum allowable absolute difference between this quantity's value and
-        ///     <paramref name="other"/> as a percentage of this quantity's value. For example, a relative tolerance
-        ///     of 0.01 means the difference must be within +/- 1% of this quantity's value to be considered equal.
+        ///     <paramref name="other"/> as a percentage of this quantity's value. <paramref name="other"/> will be converted into
+        ///     this quantity's unit for comparison. A relative tolerance of 0.01 means the difference must be within +/- 1% of
+        ///     this quantity's value to be considered equal.
+        ///     <example>
+        ///     In this example, the two quantities will be equal if the value of b is within +/- 1% of a (0.02m or 2cm).
+        ///     <code>
+        ///     var a = Length.FromMeters(2.0);
+        ///     var b = Length.FromInches(50.0);
+        ///     a.Equals(b, 0.01, ComparisonType.Relative);
+        ///     </code>
+        ///     </example>
         ///     </para>
         ///     <para>
         ///     Absolute tolerance is defined as the maximum allowable absolute difference between this quantity's value and
-        ///     <paramref name="other"/> as a fixed number.
+        ///     <paramref name="other"/> as a fixed number in this quantity's unit. <paramref name="other"/> will be converted into
+        ///     this quantity's unit for comparison.
+        ///     <example>
+        ///     In this example, the two quantities will be equal if the value of b is within 0.01 of a (0.01m or 1cm).
+        ///     <code>
+        ///     var a = Length.FromMeters(2.0);
+        ///     var b = Length.FromInches(50.0);
+        ///     a.Equals(b, 0.01, ComparisonType.Absolute);
+        ///     </code>
+        ///     </example>
         ///     </para>
         ///     <para>
         ///     Note that it is advised against specifying zero difference, due to the nature
