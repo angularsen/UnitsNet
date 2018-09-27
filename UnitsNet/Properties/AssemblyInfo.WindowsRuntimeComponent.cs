8c4e989eb86eec83343aa3f9631946e1a30e9c19
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2018-09-27 20:23:44 +0200

    UnitsNet: 4.0.0-alpha1

--- UnitsNet/Properties/AssemblyInfo.WindowsRuntimeComponent.cs
+++ UnitsNet/Properties/AssemblyInfo.WindowsRuntimeComponent.cs
@@ -38,6 +38,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("")]
-[assembly: AssemblyVersion("3.105.0")]
-[assembly: AssemblyFileVersion("3.105.0")]
+[assembly: AssemblyVersion("4.0.0")]
+[assembly: AssemblyFileVersion("4.0.0")]
 [assembly: InternalsVisibleTo("UnitsNet.WindowsRuntimeComponent.Tests")]
