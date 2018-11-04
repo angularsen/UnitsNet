3e4f1cd9b4c993da3dd9937e6bcc8a265d18c94a
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2018-11-04 21:53:35 +0100

    Regenerate

--- UnitsNet/GeneratedCode/Units/SpecificEntropyUnit.g.cs
+++ UnitsNet/GeneratedCode/Units/SpecificEntropyUnit.g.cs
@@ -9,7 +9,7 @@
 //     See https://github.com/angularsen/UnitsNet/wiki/Adding-a-New-Unit for how to add or edit units.
 //
 //     Add CustomCode\Quantities\MyQuantity.extra.cs files to add code to generated quantities.
-//     Add UnitDefinitions\MyQuantity.json and run GeneratUnits.bat to generate new units or quantities.
+//     Add UnitDefinitions\MyQuantity.json and run generate-code.bat to generate new units or quantities.
 //
 // </auto-generated>
 //------------------------------------------------------------------------------
