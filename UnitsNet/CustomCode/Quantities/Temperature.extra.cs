b0361ba24490c59bcd91383fb19c57a8babf1e6b
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2018-11-08 21:13:50 +0100

    Normalize Temperature arithmetic (#550)
    * Temperature: Generate default arithmetic
    
    * Remove Temperature.Multiply/Divide and - operator methods
    
    These are instead handled by standard arithmetic, now generated for Temperature.
    
    * Add Temperature.ToDelta(other) instance method
    
    * Add TemperatureDelta.FromTemperatures(left, right) static method
    
    * Add ToDeltaUnit()
    
    Use ToString() and Enum.Parse() to get slightly more
    robust conversion without having to generate a switch
    statement for all cases.

--- UnitsNet/CustomCode/Quantities/Temperature.extra.cs
+++ UnitsNet/CustomCode/Quantities/Temperature.extra.cs
@@ -1,16 +1,16 @@
 ﻿// Copyright (c) 2013 Andreas Gullberg Larsen (andreas.larsen84@gmail.com).
 // https://github.com/angularsen/UnitsNet
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -36,7 +36,7 @@ namespace UnitsNet
         // Windows Runtime Component does not allow operator overloads: https://msdn.microsoft.com/en-us/library/br230301.aspx
 #if !WINDOWS_UWP
         /// <summary>
-        ///     Add a <see cref="Temperature" /> and a <see cref="TemperatureDelta" />.
+        ///     Add a <see cref="TemperatureDelta" /> to a <see cref="Temperature" />.
         /// </summary>
         /// <remarks>Due to temperature units having different scales, the arithmetic must be performed on the same scale.</remarks>
         /// <returns>The new temperature.</returns>
@@ -46,7 +46,7 @@ namespace UnitsNet
         }
 
         /// <summary>
-        ///     Add a <see cref="TemperatureDelta" /> and a <see cref="Temperature" />.
+        ///     Add a <see cref="TemperatureDelta" /> to a <see cref="Temperature" />.
         /// </summary>
         /// <remarks>Due to temperature units having different scales, the arithmetic must be performed on the same scale.</remarks>
         /// <returns>The new temperature.</returns>
@@ -56,7 +56,7 @@ namespace UnitsNet
         }
 
         /// <summary>
-        ///     Subtract a <see cref="Temperature" /> by a <see cref="TemperatureDelta" />.
+        ///     Subtract a <see cref="TemperatureDelta" /> from a <see cref="Temperature" />.
         /// </summary>
         /// <remarks>Due to temperature units having different scales, the arithmetic must be performed on the same scale.</remarks>
         /// <returns>The new temperature.</returns>
@@ -66,49 +66,15 @@ namespace UnitsNet
         }
 
         /// <summary>
-        ///     Subtract a <see cref="Temperature" /> by a <see cref="TemperatureDelta" />.
+        ///     Subtract two temperatures to get a <see cref="TemperatureDelta"/> in this temperature's unit, which you can later convert to any temperature unit.
+        ///     This is useful since Celsius, Fahrenheit and Kelvins don't share a common zero point on the scale and normal subtraction
+        ///     would operate on Kelvins and likely give an unexpected result.
+        ///     Example:
+        ///     double deltaCelsius = celsius30.ToDelta(celsius20).DegreesCelsius; // 10 C
+        ///     double wrongDeltaCelsius = (celsius30 - celsius20).DegreesCelsius; // 303.15 K - 293.15 K = 10 K = -263.15 degrees Celsius
         /// </summary>
-        /// <remarks>Due to temperature units having different scales, the arithmetic must be performed on the same scale.</remarks>
-        /// <returns>The delta temperature (difference).</returns>
-        public static TemperatureDelta operator -(Temperature left, Temperature right)
-        {
-            return new TemperatureDelta(left.Kelvins - right.Kelvins, TemperatureDeltaUnit.Kelvin);
-        }
+        /// <returns>A temperature delta.</returns>
+        public TemperatureDelta ToDelta(Temperature other) => TemperatureDelta.FromTemperatures(this, other);
 #endif
-
-        /// <summary>
-        ///     Multiply temperature with a <paramref name="factor" /> in a given <paramref name="unit" />.
-        /// </summary>
-        /// <remarks>
-        ///     Due to different temperature units having different zero points, we cannot simply
-        ///     multiply or divide a temperature by a factor. We must first convert to the desired unit, then perform the
-        ///     calculation.
-        /// </remarks>
-        /// <param name="factor">Factor to multiply by.</param>
-        /// <param name="unit">Unit to perform multiplication in.</param>
-        /// <returns>The resulting <see cref="Temperature" />.</returns>
-        public Temperature Multiply(double factor, TemperatureUnit unit)
-        {
-            double resultInUnit = As(unit) * factor;
-            return From(resultInUnit, unit);
-        }
-
-
-        /// <summary>
-        ///     Divide temperature by a <paramref name="divisor" /> in a given <paramref name="unit" />.
-        /// </summary>
-        /// <remarks>
-        ///     Due to different temperature units having different zero points, we cannot simply
-        ///     multiply or divide a temperature by a factor. We must first convert to the desired unit, then perform the
-        ///     calculation.
-        /// </remarks>
-        /// <param name="divisor">Factor to multiply by.</param>
-        /// <param name="unit">Unit to perform multiplication in.</param>
-        /// <returns>The resulting <see cref="Temperature" />.</returns>
-        public Temperature Divide(double divisor, TemperatureUnit unit)
-        {
-            double resultInUnit = As(unit) / divisor;
-            return From(resultInUnit, unit);
-        }
     }
 }
--- UnitsNet/CustomCode/Quantities/TemperatureDelta.extra.cs
+++ UnitsNet/CustomCode/Quantities/TemperatureDelta.extra.cs
@@ -20,6 +20,10 @@
 // THE SOFTWARE.
 
 // ReSharper disable once CheckNamespace
+
+using System;
+using UnitsNet.Units;
+
 namespace UnitsNet
 {
     // Windows Runtime Component has constraints on public types: https://msdn.microsoft.com/en-us/library/br230301.aspx#Declaring types in Windows Runtime Components
@@ -46,6 +50,32 @@ namespace UnitsNet
         public static SpecificEnergy operator *(TemperatureDelta temperatureDelta, SpecificEntropy specificEntropy)
         {
             return specificEntropy * temperatureDelta;
+        }
+
+        /// <summary>
+        ///     Subtract two temperatures to get a <see cref="TemperatureDelta"/> in <paramref name="left"/>'s unit, which you can later convert to any temperature unit.
+        ///     This is useful since Celsius, Fahrenheit and Kelvins don't share a common zero point on the scale and normal subtraction
+        ///     would operate on Kelvins and likely give an unexpected result.
+        ///     Example:
+        ///     double deltaCelsius = TemperatureDelta.FromTemperatures(celsius30, celsius20).DegreesCelsius; // 10 C
+        ///     double wrongDeltaCelsius = (celsius30 - celsius20).DegreesCelsius; // 303.15 K - 293.15 K = 10 K = -263.15 degrees Celsius
+        /// </summary>
+        /// <returns>A temperature delta.</returns>
+        public static TemperatureDelta FromTemperatures(Temperature left, Temperature right)
+        {
+            TemperatureDeltaUnit deltaUnit = ToDeltaUnit(left.Unit);
+            return new TemperatureDelta(left.Value - right.As(left.Unit), deltaUnit);
+        }
+
+        /// <summary>
+        /// Converts a temperature unit to a temperature delta unit.
+        /// They share the exact same unit names, but are of different enum types and have slightly different semantic meaning.
+        /// </summary>
+        /// <param name="temperatureUnit">Temperature unit.</param>
+        /// <returns>Equivalent temperature delta unit.</returns>
+        public static TemperatureDeltaUnit ToDeltaUnit(TemperatureUnit temperatureUnit)
+        {
+            return (TemperatureDeltaUnit)Enum.Parse(typeof(TemperatureDeltaUnit), temperatureUnit.ToString());
         }
 #endif
     }
