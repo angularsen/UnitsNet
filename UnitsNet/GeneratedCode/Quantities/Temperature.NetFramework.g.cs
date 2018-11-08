b7250930722ff5c0d84c8d0769d0c7d1f2c8d8a3
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2018-11-08 23:15:02 +0100

    Fix Temperature arithmetic manually

--- UnitsNet/GeneratedCode/Quantities/Temperature.NetFramework.g.cs
+++ UnitsNet/GeneratedCode/Quantities/Temperature.NetFramework.g.cs
@@ -454,12 +454,12 @@ namespace UnitsNet
 
         public static Temperature operator +(Temperature left, Temperature right)
         {
-            return new Temperature(left.Value + right.AsBaseNumericType(left.Unit), left.Unit);
+            return From(left.AsBaseUnit() + right.AsBaseUnit(), BaseUnit).ToUnit(left.Unit);
         }
 
         public static Temperature operator -(Temperature left, Temperature right)
         {
-            return new Temperature(left.Value - right.AsBaseNumericType(left.Unit), left.Unit);
+            return From(left.AsBaseUnit() - right.AsBaseUnit(), BaseUnit).ToUnit(left.Unit);
         }
 
         public static Temperature operator *(double left, Temperature right)
@@ -506,12 +506,12 @@ namespace UnitsNet
             return left.Value > right.AsBaseNumericType(left.Unit);
         }
 
-        public static bool operator ==(Temperature left, Temperature right)	
+        public static bool operator ==(Temperature left, Temperature right)
         {
             return left.Equals(right);
         }
 
-        public static bool operator !=(Temperature left, Temperature right)	
+        public static bool operator !=(Temperature left, Temperature right)
         {
             return !(left == right);
         }
