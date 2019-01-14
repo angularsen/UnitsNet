bbbd39d98d231781e50882cfd3c1e951345f7027
ngeslin <nathan.geslin@free.fr>
2019-01-14 14:26:40 +0100

    UnitsNet: 4.7.0

--- UnitsNet/Properties/AssemblyInfo.WindowsRuntimeComponent.cs
+++ UnitsNet/Properties/AssemblyInfo.WindowsRuntimeComponent.cs
@@ -38,6 +38,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("")]
-[assembly: AssemblyVersion("4.6.0")]
-[assembly: AssemblyFileVersion("4.6.0")]
+[assembly: AssemblyVersion("4.7.0")]
+[assembly: AssemblyFileVersion("4.7.0")]
 [assembly: InternalsVisibleTo("UnitsNet.WindowsRuntimeComponent.Tests")]
