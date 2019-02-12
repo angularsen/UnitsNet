98eb2576259643836108b174c09b5de343d322d4
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2019-02-13 00:11:11 +0100

    Simplify NetFramework scripts

--- UnitsNet/GeneratedCode/Quantities/MassMomentOfInertia.NetFramework.g.cs
+++ UnitsNet/GeneratedCode/Quantities/MassMomentOfInertia.NetFramework.g.cs
@@ -72,7 +72,6 @@ namespace UnitsNet
         /// </summary>
         /// <param name="numericValue">The numeric value  to contruct this quantity with.</param>
         /// <param name="unit">The unit representation to contruct this quantity with.</param>
-        /// <remarks>Value parameter cannot be named 'value' due to constraint when targeting Windows Runtime Component.</remarks>
         /// <exception cref="ArgumentException">If value is NaN or Infinity.</exception>
         public MassMomentOfInertia(double numericValue, MassMomentOfInertiaUnit unit)
         {
@@ -816,7 +815,6 @@ namespace UnitsNet
             return CompareTo(objMassMomentOfInertia);
         }
 
-        // Windows Runtime Component does not allow public methods/ctors with same number of parameters: https://msdn.microsoft.com/en-us/library/br230301.aspx#Overloaded methods
         public int CompareTo(MassMomentOfInertia other)
         {
             return _value.CompareTo(other.AsBaseNumericType(this.Unit));
--- UnitsNet/GeneratedCode/Quantities/Volume.NetFramework.g.cs
+++ UnitsNet/GeneratedCode/Quantities/Volume.NetFramework.g.cs
@@ -72,7 +72,6 @@ namespace UnitsNet
         /// </summary>
         /// <param name="numericValue">The numeric value  to contruct this quantity with.</param>
         /// <param name="unit">The unit representation to contruct this quantity with.</param>
-        /// <remarks>Value parameter cannot be named 'value' due to constraint when targeting Windows Runtime Component.</remarks>
         /// <exception cref="ArgumentException">If value is NaN or Infinity.</exception>
         public Volume(double numericValue, VolumeUnit unit)
         {
@@ -1054,7 +1053,6 @@ namespace UnitsNet
             return CompareTo(objVolume);
         }
 
-        // Windows Runtime Component does not allow public methods/ctors with same number of parameters: https://msdn.microsoft.com/en-us/library/br230301.aspx#Overloaded methods
         public int CompareTo(Volume other)
         {
             return _value.CompareTo(other.AsBaseNumericType(this.Unit));
