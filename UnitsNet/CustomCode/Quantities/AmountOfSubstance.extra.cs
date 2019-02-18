a428c75b0bea8d4d3747047f873448b5eda36327
Tristan Milnthorp <tmilnthorp@gmail.com>
2019-02-18 14:05:43 -0500

    Removing WRC checks after WRC split

--- UnitsNet/CustomCode/Quantities/AmountOfSubstance.extra.cs
+++ UnitsNet/CustomCode/Quantities/AmountOfSubstance.extra.cs
@@ -21,19 +21,11 @@
 
 // ReSharper disable once CheckNamespace
 
-using System;
 using UnitsNet.Units;
 
 namespace UnitsNet
 {
-    // Windows Runtime Component has constraints on public types: https://msdn.microsoft.com/en-us/library/br230301.aspx#Declaring types in Windows Runtime Components
-    // Public structures can't have any members other than public fields, and those fields must be value types or strings.
-    // Public classes must be sealed (NotInheritable in Visual Basic). If your programming model requires polymorphism, you can create a public interface and implement that interface on the classes that must be polymorphic.
-#if WINDOWS_UWP
-    public sealed partial class AmountOfSubstance
-#else
     public partial struct AmountOfSubstance
-#endif
     {
         public static double AvogadroConstant { get; } = 6.02214076e23;
 
