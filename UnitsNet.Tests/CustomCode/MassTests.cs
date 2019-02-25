5454102266e755ba7ae7c96d2774af7875d30324
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2019-02-25 22:18:09 +0100

    Change license to MIT-0 (#615)
    * Change license from MIT to MIT-0 and fix nuget warning
    
    The difference is to remove the requirement that the license must be copied with all copies of the work.
    I don't want to impose this on anyone wanting to take inspiration from the code.
    
    Use <licenseExpression> instead of the deprecated <licenseUrl> to fix NuGet warning when packing nugets.
    https://github.com/dotnet/docs/blob/master/docs/core/tools/csproj.md#packagelicenseexpression
    
    * Update copyright header of all .ps1 files
    
    * Regen code with new copyright header
    
    * Replace headers in non-generated code
    
    * R#: Update default file headers
    
    * Remove duplicate UnitsNet.csproj, in WRC folder
    
    * Update assembly copyrights
    
    * Update nugets' copyright to be consistent
    
    * Update Tools\NuGet.exe
    
    To add support for new licenseExpression field in nuspec.
    
    * Fix license expression in WRC nuspec

--- UnitsNet.Tests/CustomCode/MassTests.cs
+++ UnitsNet.Tests/CustomCode/MassTests.cs
@@ -1,23 +1,5 @@
-﻿// Copyright (c) 2013 Andreas Gullberg Larsen (andreas.larsen84@gmail.com).
-// https://github.com/angularsen/UnitsNet
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
+﻿// Licensed under MIT No Attribution, see LICENSE file at the root.
+// Copyright 2013 Andreas Gullberg Larsen (andreas.larsen84@gmail.com). Maintained at https://github.com/angularsen/UnitsNet.
 
 using System;
 using UnitsNet.Units;
@@ -70,7 +52,7 @@ namespace UnitsNet.Tests.CustomCode
         protected override double StoneInOneKilogram => 0.1574731728702698;
 
         protected override double TonnesInOneKilogram => 1E-3;
-        
+
         protected override double LongHundredweightInOneKilogram => 0.01968413055222121;
 
         protected override double ShortHundredweightInOneKilogram => 0.022046226218487758;
