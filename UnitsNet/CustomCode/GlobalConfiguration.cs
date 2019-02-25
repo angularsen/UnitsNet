dba20cb9ff1e0e97f42308ea979d5014af6707b4
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2019-02-25 22:50:15 +0100

    Obsolete GlobalConfiguration

--- UnitsNet/CustomCode/GlobalConfiguration.cs
+++ UnitsNet/CustomCode/GlobalConfiguration.cs
@@ -7,12 +7,18 @@ using System.Globalization;
 // ReSharper disable once CheckNamespace
 namespace UnitsNet
 {
+    /// <summary>
+    ///     Global configuration for culture, used as default culture in methods like <see cref="Length.ToString()" /> and
+    ///     <see cref="Length.Parse(string)" />.
+    /// </summary>
+    [Obsolete("The only property DefaultCulture is now deprecated. Manipulate CultureInfo.CurrentUICulture instead.")]
     public sealed class GlobalConfiguration
     {
         /// <summary>
         ///     Defaults to <see cref="CultureInfo.CurrentUICulture" /> when creating an instance with no culture provided.
         ///     Can be overridden, but note that this is static and will affect all subsequent usages.
         /// </summary>
+        [Obsolete("Manipulate CultureInfo.CurrentUICulture instead, this property will be removed.")]
         public static IFormatProvider DefaultCulture { get; set; } = CultureInfo.CurrentUICulture;
     }
 }
