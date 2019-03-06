0c7a2f4131be7befade6b6ea452465e52a786370
Tristan Milnthorp <tmilnthorp@gmail.com>
2019-03-06 16:57:45 -0500

    Adding benchmark project

---
+++ UnitsNet.Benchmark/Program.cs
@@ -0,0 +1,49 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using UnitsNet.Units;
+
+namespace UnitsNet.Benchmark
+{
+    public class UnitsNetBenchmarks
+    {
+        private Length length = Length.FromMeters(3.0);
+
+        [Benchmark]
+        public Length Constructor() => new Length(3.0, LengthUnit.Meter);
+
+        [Benchmark]
+        public Length FromMethod() => Length.FromMeters( 3.0 );
+
+        [Benchmark]
+        public double ToProperty() => length.Centimeters;
+
+        [Benchmark]
+        public double As() => length.As(LengthUnit.Centimeter);
+
+        [Benchmark]
+        public Length ToUnit() => length.ToUnit( LengthUnit.Centimeter );
+
+        [Benchmark]
+        public string ToStringTest() => length.ToString();
+
+        [Benchmark]
+        public Length Parse() => Length.Parse("3.0 m");
+
+        [Benchmark]
+        public bool TryParseValid() => Length.TryParse( "3.0 m", out var l );
+
+        [Benchmark]
+        public bool TryParseInvalid() => Length.TryParse( "3.0 zoom", out var l );
+
+        [Benchmark]
+        public IQuantity QuantityFrom() => Quantity.From( 3.0, LengthUnit.Meter );
+    }
+
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var summary = BenchmarkRunner.Run<UnitsNetBenchmarks>();
+        }
+    }
+}
