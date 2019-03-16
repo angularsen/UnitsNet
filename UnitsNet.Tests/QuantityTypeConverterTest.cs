fba61c2c85f36aa6ff4ff669ef6ba357a6e215dc
Wilhelm Wiens <wiwie15@student.sdu.dk>
2019-03-17 00:08:19 +0100

    Initial implementation of UnitsNetTypeConverter

---
+++ UnitsNet.Tests/QuantityTypeConverterTest.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using System.Text;
+using UnitsNet;
+using Xunit;
+
+namespace UnitsNet.Tests
+{
+    public class QuantityTypeConverterTest
+    {
+        // https://stackoverflow.com/questions/3612909/why-is-this-typeconverter-not-working
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            AppDomain domain = (AppDomain)sender;
+            foreach (Assembly asm in domain.GetAssemblies())
+            {
+                if (asm.FullName == args.Name)
+                {
+                    return asm;
+                }
+            }
+            return null;
+        }
+
+        static QuantityTypeConverterTest()
+        {
+            // NOTE: After this, you can use your typeconverter.
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+        }
+
+        static CultureInfo culture = CultureInfo.GetCultureInfo("en-US");
+
+        public class TypeDescriptorContext : ITypeDescriptorContext
+        {
+            public class PropertyDescriptor_ : PropertyDescriptor
+            {
+                public PropertyDescriptor_(string name, Attribute[] attributes) : base(name, attributes)
+                {
+
+                }
+
+                public override Type ComponentType => throw new NotImplementedException();
+
+                public override bool IsReadOnly => throw new NotImplementedException();
+
+                public override Type PropertyType => throw new NotImplementedException();
+
+                public override bool CanResetValue(object component)
+                {
+                    throw new NotImplementedException();
+                }
+
+                public override object GetValue(object component)
+                {
+                    throw new NotImplementedException();
+                }
+
+                public override void ResetValue(object component)
+                {
+                    throw new NotImplementedException();
+                }
+
+                public override void SetValue(object component, object value)
+                {
+                    throw new NotImplementedException();
+                }
+
+                public override bool ShouldSerializeValue(object component)
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public TypeDescriptorContext(string name, Attribute[] attributes)
+            {
+                PropertyDescriptor = new PropertyDescriptor_(name, attributes);
+            }
+
+            public IContainer Container => throw new NotImplementedException();
+
+            public object Instance => throw new NotImplementedException();
+
+            public PropertyDescriptor PropertyDescriptor { get; set; }
+
+            public object GetService(Type serviceType)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void OnComponentChanged()
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool OnComponentChanging()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [Theory]
+        [InlineData(typeof(string))]
+        public void CanConvertFrom_GiveSomeTypes_ReturnsTrue(Type value)
+        {
+            UnitsNetTypeConverter<Length> unitsNETTypeConverter = new UnitsNetTypeConverter<Length>();
+
+            Assert.True(unitsNETTypeConverter.CanConvertFrom(value));
+        }
+
+        [Theory]
+        [InlineData(typeof(double))]
+        [InlineData(typeof(object))]
+        [InlineData(typeof(float))]
+        [InlineData(typeof(Length))]
+        public void CanConvertFrom_GiveSomeTypes_ReturnsFalse(Type value)
+        {
+            UnitsNetTypeConverter<Length> unitsNETTypeConverter = new UnitsNetTypeConverter<Length>();
+
+            Assert.False(unitsNETTypeConverter.CanConvertFrom(value));
+        }
+
+        [Theory]
+        [InlineData(typeof(string))]
+        public void CanConvertTo_GiveSomeTypes_ReturnsTrue(Type value)
+        {
+            UnitsNetTypeConverter<Length> unitsNETTypeConverter = new UnitsNetTypeConverter<Length>();
+
+            Assert.True(unitsNETTypeConverter.CanConvertTo(value));
+        }
+
+        [Theory]
+        [InlineData(typeof(double))]
+        [InlineData(typeof(object))]
+        [InlineData(typeof(float))]
+        [InlineData(typeof(Length))]
+        public void CanConvertTo_GiveSomeTypes_ReturnsFalse(Type value)
+        {
+            UnitsNetTypeConverter<Length> unitsNETTypeConverter = new UnitsNetTypeConverter<Length>();
+
+            Assert.False(unitsNETTypeConverter.CanConvertTo(value));
+        }
+
+        [Fact]
+        public void ConvertFrom_GiveSomeStrings()
+        {
+            UnitsNetTypeConverter<Length> unitsNETTypeConverter = new UnitsNetTypeConverter<Length>();
+            ITypeDescriptorContext context = new TypeDescriptorContext("SomeMemberName", new Attribute[] { });
+
+            Assert.Equal(Length.FromMillimeters(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1mm"));
+            Assert.Equal(Length.FromMeters(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1m"));
+            Assert.Equal(Length.FromMeters(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1"));
+            Assert.Equal(Length.FromKilometers(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1km"));
+
+            context = new TypeDescriptorContext("SomeMemberName", new Attribute[] { new DefaultUnitAttribute(Units.LengthUnit.Centimeter) });
+
+            Assert.Equal(Length.FromMillimeters(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1mm"));
+            Assert.Equal(Length.FromMeters(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1m"));
+            Assert.Equal(Length.FromCentimeters(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1"));
+            Assert.Equal(Length.FromKilometers(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1km"));
+
+            context = new TypeDescriptorContext("SomeMemberName", new Attribute[]
+                {
+                    new DefaultUnitAttribute(Units.LengthUnit.Centimeter),
+                    new ConvertToUnitAttribute(Units.LengthUnit.Meter)
+                });
+
+            Assert.Equal(Length.FromMeters(0.001), unitsNETTypeConverter.ConvertFrom(context, culture, "1mm"));
+            Assert.Equal(Length.FromMeters(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1m"));
+            Assert.Equal(Length.FromMeters(0.01), unitsNETTypeConverter.ConvertFrom(context, culture, "1"));
+            Assert.Equal(Length.FromMeters(1000), unitsNETTypeConverter.ConvertFrom(context, culture, "1km"));
+        }
+
+        [Fact]
+        public void ConvertFrom_GiveEmpyString()
+        {
+            UnitsNetTypeConverter<Length> unitsNETTypeConverter = new UnitsNetTypeConverter<Length>();
+            ITypeDescriptorContext context = new TypeDescriptorContext("SomeMemberName", new Attribute[] { });
+
+            Assert.Throws<NotSupportedException>(() => unitsNETTypeConverter.ConvertFrom(context, culture, ""));
+        }
+
+        [Fact]
+        public void ConvertFrom_GiveWrongQuantity()
+        {
+            UnitsNetTypeConverter<Length> unitsNETTypeConverter = new UnitsNetTypeConverter<Length>();
+            ITypeDescriptorContext context = new TypeDescriptorContext("SomeMemberName", new Attribute[] { });
+
+            Assert.Throws<ArgumentException>(() => unitsNETTypeConverter.ConvertFrom(context, culture, "1m^2"));
+        }
+
+        [Fact]
+        public void WrongUnitTypeInAttribut_DefaultUnit()
+        {
+            UnitsNetTypeConverter<Length> unitsNETTypeConverter = new UnitsNetTypeConverter<Length>();
+            ITypeDescriptorContext context = new TypeDescriptorContext("SomeMemberName", new Attribute[] { new DefaultUnitAttribute(Units.VolumeUnit.CubicMeter) });
+
+            Assert.Throws<InvalidOperationException>(() => unitsNETTypeConverter.ConvertFrom(context, culture, "1"));
+        }
+
+        [Fact]
+        public void ConvertFrom_GiveStringWithPower_1()
+        {
+            UnitsNetTypeConverter<Length> unitsNETTypeConverter = new UnitsNetTypeConverter<Length>();
+            ITypeDescriptorContext context = new TypeDescriptorContext("SomeMemberName", new Attribute[] { });
+
+            Assert.Equal(Length.FromMeters(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1m"));
+            Assert.Equal(Length.FromMeters(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1m^1"));
+        }
+
+        [Fact]
+        public void ConvertFrom_GiveStringWithPower_2()
+        {
+            UnitsNetTypeConverter<Area> unitsNETTypeConverter = new UnitsNetTypeConverter<Area>();
+            ITypeDescriptorContext context = new TypeDescriptorContext("SomeMemberName", new Attribute[] { });
+
+            Assert.Equal(Area.FromSquareMeters(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1m²"));
+            Assert.Equal(Area.FromSquareMeters(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1m^2"));
+        }
+
+        [Fact]
+        public void ConvertFrom_GiveStringWithPower_3()
+        {
+            UnitsNetTypeConverter<Volume> unitsNETTypeConverter = new UnitsNetTypeConverter<Volume>();
+            ITypeDescriptorContext context = new TypeDescriptorContext("SomeMemberName", new Attribute[] { });
+
+            Assert.Equal(Volume.FromCubicMeters(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1m³"));
+            Assert.Equal(Volume.FromCubicMeters(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1m^3"));
+        }
+
+        [Fact]
+        public void ConvertFrom_GiveStringWithPower_4()
+        {
+            UnitsNetTypeConverter<AreaMomentOfInertia> unitsNETTypeConverter = new UnitsNetTypeConverter<AreaMomentOfInertia>();
+            ITypeDescriptorContext context = new TypeDescriptorContext("SomeMemberName", new Attribute[] { });
+
+            Assert.Equal(AreaMomentOfInertia.FromMetersToTheFourth(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1m⁴"));
+            Assert.Equal(AreaMomentOfInertia.FromMetersToTheFourth(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1m^4"));
+        }
+
+        [Fact]
+        public void ConvertFrom_GiveStringWithPower_minus1()
+        {
+            UnitsNetTypeConverter<CoefficientOfThermalExpansion> unitsNETTypeConverter = new UnitsNetTypeConverter<CoefficientOfThermalExpansion>();
+            ITypeDescriptorContext context = new TypeDescriptorContext("SomeMemberName", new Attribute[] { });
+
+            Assert.Equal(CoefficientOfThermalExpansion.FromInverseKelvin(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1K⁻¹"));
+            Assert.Equal(CoefficientOfThermalExpansion.FromInverseKelvin(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1K^-1"));
+        }
+
+        [Fact]
+        public void ConvertFrom_GiveStringWithPower_minus2()
+        {
+            UnitsNetTypeConverter<MassFlux> unitsNETTypeConverter = new UnitsNetTypeConverter<MassFlux>();
+            ITypeDescriptorContext context = new TypeDescriptorContext("SomeMemberName", new Attribute[] { });
+
+            Assert.Equal(MassFlux.FromKilogramsPerSecondPerSquareMeter(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1kg·s⁻¹·m⁻²"));
+            Assert.Equal(MassFlux.FromKilogramsPerSecondPerSquareMeter(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1kg·s^-1·m^-2"));
+            Assert.Equal(MassFlux.FromKilogramsPerSecondPerSquareMeter(1), unitsNETTypeConverter.ConvertFrom(context, culture, "1kg*s^-1*m^-2"));
+        }
+    }
+}
---
+++ UnitsNet/QuantityTypeConverter.cs
@@ -0,0 +1,205 @@
+﻿// Licensed under MIT No Attribution, see LICENSE file at the root.
+// Copyright 2013 Andreas Gullberg Larsen (andreas.larsen84@gmail.com). Maintained at https://github.com/angularsen/UnitsNet.
+
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace UnitsNet
+{
+    /// <summary>
+    /// Is the base class for all attributes that are related to <see cref="UnitsNetTypeConverter{T}"/>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public abstract class UnitAttributeBase : Attribute
+    {
+        /// <summary>
+        /// This property hold the UnitType information
+        /// </summary>
+        public Enum UnitType { get; set; }
+
+        /// <summary>
+        /// Constructor of this class
+        /// </summary>
+        /// <param name="unitType"></param>
+        public UnitAttributeBase(object unitType)
+        {
+            UnitType = unitType as Enum;
+        }
+    }
+
+    /// <summary>
+    /// This attribute defines the default Unit to use. It is only used when the string only consists of digits
+    /// </summary>
+    public class DefaultUnitAttribute : UnitAttributeBase
+    {
+        /// <summary>
+        /// Constructor for ConvertToUnit attribute
+        /// </summary>
+        /// <param name="unitType">The unit the quantity gets when the string parsing dose only consist of digits</param>
+        public DefaultUnitAttribute(object unitType) : base(unitType) { }
+    }
+
+    /// <summary>
+    /// This attribute defines the Unit the quantity is converted to after it has been parsed.
+    /// </summary>
+    public class ConvertToUnitAttribute : DefaultUnitAttribute
+    {
+        /// <summary>
+        /// Constructor for ConvertToUnit attribute
+        /// </summary>
+        /// <param name="unitType">The unit the quantity is converted to when parsing from string</param>
+        public ConvertToUnitAttribute(object unitType) : base(unitType) { }
+    }
+
+    /// <summary>
+    /// This attribute defines the unit the quantity has when converting to string
+    /// </summary>
+    public class DisplayAsUnitAttribute : DefaultUnitAttribute
+    {
+        /// <summary>
+        /// The formating used when the quantity is converted to string
+        /// </summary>
+        public string Format { get; set; }
+
+        /// <summary>
+        /// Constructor for DisplayAsUnit attribute
+        /// </summary>
+        /// <param name="unitType">The unit the quantity should be displayed in</param>
+        /// <param name="format">Formating string <see cref="IQuantity.ToString(System.IFormatProvider)"/></param>
+        public DisplayAsUnitAttribute(object unitType, string format = "") : base(unitType) { Format = format; }
+    }
+
+    /// <summary>
+    ///     Converts IQuantitys from string and to string.
+    ///     Implements the TypeConverter interface so that eg the PropertyGrid can read and write the properties implementing the IQuantity interface.
+    /// </summary>
+    /// <typeparam name="TQuantity">Quantity value type, such as <see cref="Length"/> or <see cref="Mass"/>.</typeparam>
+    public class UnitsNetTypeConverter<TQuantity> : TypeConverter where TQuantity : IQuantity
+    {
+        /// <summary>
+        ///     Returns whether this converter can convert an object of the given type to the type of this converter, using the specified context.
+        /// </summary>
+        /// <param name="context">An System.ComponentModel.ITypeDescriptorContext that provides a format context.</param>
+        /// <param name="sourceType">A System.Type that represents the type you want to convert from.</param>
+        /// <returns>true if this converter can perform the conversion; otherwise, false.</returns>
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return (sourceType == typeof(string)) || base.CanConvertFrom(context, sourceType);
+        }
+
+        private static TAttribute GetAttribute<TAttribute>(ITypeDescriptorContext context) where TAttribute : UnitAttributeBase
+        {
+            TAttribute attribute = null;
+            AttributeCollection ua = context?.PropertyDescriptor.Attributes;
+
+            attribute = (TAttribute)ua?[typeof(TAttribute)];
+
+            if(attribute != null)
+            {
+                QuantityType expected = default(TQuantity).Type;
+                QuantityType actual = Quantity.From(1, attribute.UnitType).Type;
+                if(expected != actual)
+                {
+                    throw new InvalidOperationException($"The specified UnitType:'{attribute.UnitType}' dose not match QuantityType:'{expected}'");
+                }
+            }
+
+            return attribute;
+        }
+
+        /// <summary>
+        ///     Converts the given object to the type of this converter, using the specified context and culture information.
+        /// </summary>
+        /// <param name="context">An System.ComponentModel.ITypeDescriptorContext that provides a format context.</param>
+        /// <param name="culture">The System.Globalization.CultureInfo to use as the current culture.</param>
+        /// <param name="value">The System.Object to convert.</param>
+        /// <returns>An <see cref="IQuantity"/> object.</returns>
+        /// <exception cref="System.NotSupportedException">The conversion cannot be performed.</exception>
+        /// <exception cref="ArgumentException">Unit value is not a know unit enum type.</exception>
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            string stringValue = value as string;
+            object result = null;
+
+            if (!string.IsNullOrEmpty(stringValue))
+            {
+                if (double.TryParse(stringValue, NumberStyles.Any, culture, out double dvalue))
+                {
+                    DefaultUnitAttribute defaultUnit = GetAttribute<DefaultUnitAttribute>(context) ?? new DefaultUnitAttribute(default(TQuantity).Unit);
+
+                    result = Quantity.From(dvalue, defaultUnit.UnitType);
+                }
+                else
+                {
+                    // this should not be part of QuantityTypeConverter. it should rather be part of the parse function
+                    stringValue = stringValue.Replace("^-9", "⁻⁹"); 
+                    stringValue = stringValue.Replace("^-8", "⁻⁸");
+                    stringValue = stringValue.Replace("^-7", "⁻⁷");
+                    stringValue = stringValue.Replace("^-6", "⁻⁶");
+                    stringValue = stringValue.Replace("^-5", "⁻⁵");
+                    stringValue = stringValue.Replace("^-4", "⁻⁴");
+                    stringValue = stringValue.Replace("^-3", "⁻³");
+                    stringValue = stringValue.Replace("^-2", "⁻²");
+                    stringValue = stringValue.Replace("^-1", "⁻¹");
+                    stringValue = stringValue.Replace("^1", "");
+                    stringValue = stringValue.Replace("^2", "²");
+                    stringValue = stringValue.Replace("^3", "³");
+                    stringValue = stringValue.Replace("^4", "⁴");
+                    stringValue = stringValue.Replace("^5", "⁵");
+                    stringValue = stringValue.Replace("^6", "⁶");
+                    stringValue = stringValue.Replace("^7", "⁷");
+                    stringValue = stringValue.Replace("^8", "⁸");
+                    stringValue = stringValue.Replace("^9", "⁹");
+                    stringValue = stringValue.Replace("*", "·");
+
+                    result = Quantity.Parse(culture, typeof(TQuantity), stringValue);
+                }
+
+                ConvertToUnitAttribute convertToUnit = GetAttribute<ConvertToUnitAttribute>(context);
+                if (convertToUnit != null)
+                {
+                    result = ((IQuantity)result).ToUnit(convertToUnit.UnitType);
+                }
+            }
+
+            return result ?? base.ConvertFrom(context, culture, value);
+        }
+
+        /// <summary>Returns whether this converter can convert the object to the specified type, using the specified context.</summary>
+        /// <returns>true if this converter can perform the conversion; otherwise, false.</returns>
+        /// <param name="context">An <see cref="T:System.ComponentModel.ITypeDescriptorContext" /> that provides a format context. </param>
+        /// <param name="destinationType">A <see cref="T:System.Type" /> that represents the type you want to convert to. </param>
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+        {
+            return (destinationType == typeof(string)) || base.CanConvertTo(context, destinationType);
+        }
+
+        /// <summary>Converts the given value object to the specified type, using the specified context and culture information.</summary>
+        /// <returns>An <see cref="T:System.Object" /> that represents the converted value.</returns>
+        /// <param name="context">An <see cref="T:System.ComponentModel.ITypeDescriptorContext" /> that provides a format context. </param>
+        /// <param name="culture">A <see cref="T:System.Globalization.CultureInfo" />. If null is passed, the current culture is assumed. </param>
+        /// <param name="value">The <see cref="T:System.Object" /> to convert. </param>
+        /// <param name="destinationType">The <see cref="T:System.Type" /> to convert the <paramref name="value" /> parameter to. </param>
+        /// <exception cref="T:System.ArgumentNullException">The <paramref name="destinationType" /> parameter is null. </exception>
+        /// <exception cref="T:System.NotSupportedException">The conversion cannot be performed. </exception>
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            IQuantity qvalue = value as IQuantity;
+            object result = null;
+            DisplayAsUnitAttribute displayAsUnit = GetAttribute<DisplayAsUnitAttribute>(context);
+
+            if (destinationType == typeof(string) && qvalue != null && displayAsUnit != null)
+            {
+                result = qvalue.ToUnit(displayAsUnit.UnitType).ToString(displayAsUnit.Format, culture);
+            }
+            else
+            {
+                result = base.ConvertTo(context, culture, value, destinationType);
+            }
+
+            return result;
+        }
+    }
+}
