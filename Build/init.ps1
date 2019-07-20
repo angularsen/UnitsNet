475495f87f3da3f88e8aa018ac827efd92e3e6bd
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2019-07-20 14:59:53 +0200

    init: Download xunit-cli and reportgenerator tools

--- Build/init.ps1
+++ Build/init.ps1
@@ -18,6 +18,18 @@ if (-not (Test-Path "$root/Tools/OpenCover/OpenCover.Console.exe")) {
   Write-Host -Foreground Green "OpenCover downloaded."
 }
 
+if (-not (Test-Path "$root/Tools/xunit.exe")) {
+  Write-Host -Foreground Blue "Download xunit-cli..."
+  dotnet tool install xunit-cli --add-source https://myget.org/F/natemcmaster/api/v3/index.json --tool-path Tools
+  Write-Host -Foreground Green "Download xunit-cli...OK."
+}
+
+if (-not (Test-Path "$root/Tools/reportgenerator.exe")) {
+  Write-Host -Foreground Blue "Download dotnet-reportgenerator-globaltool..."
+  dotnet tool install dotnet-reportgenerator-globaltool --tool-path Tools
+  Write-Host -Foreground Green "Download dotnet-reportgenerator-globaltool...OK."
+}
+
 # Cleanup
 [system.io.Directory]::Delete($tempDir, $true) | out-null
 
