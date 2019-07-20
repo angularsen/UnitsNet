b951c8f4dbce29985f13b9a98bbe86595e0fc905
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2019-07-20 15:17:15 +0200

    Cleanup

--- Build/init.ps1
+++ Build/init.ps1
@@ -8,22 +8,6 @@ Write-Host -Foreground Blue "Initializing..."
 $tempDir = "$root/Tools/Temp"
 [system.io.Directory]::CreateDirectory($tempDir) | out-null
 
-if (-not (Test-Path "$root/Tools/OpenCover/OpenCover.Console.exe")) {
-  Write-Host -Foreground Blue "Downloading OpenCover..."
-  # Workaround for GitHub requiring TLS1.2 and PowerShell defaulting to TLS1:
-  # https://stackoverflow.com/a/48030563/134761
-  [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-  wget https://github.com/OpenCover/opencover/releases/download/4.7.922/opencover.4.7.922.zip -OutFile "$tempDir/opencover.zip"
-  Expand-Archive "$tempDir/opencover.zip" -DestinationPath "$root/Tools/opencover/" -Force
-  Write-Host -Foreground Green "OpenCover downloaded."
-}
-
-if (-not (Test-Path "$root/Tools/xunit.exe")) {
-  Write-Host -Foreground Blue "Download xunit-cli..."
-  dotnet tool install xunit-cli --add-source https://myget.org/F/natemcmaster/api/v3/index.json --tool-path Tools
-  Write-Host -Foreground Green "Download xunit-cli...OK."
-}
-
 if (-not (Test-Path "$root/Tools/reportgenerator.exe")) {
   Write-Host -Foreground Blue "Download dotnet-reportgenerator-globaltool..."
   dotnet tool install dotnet-reportgenerator-globaltool --tool-path Tools
