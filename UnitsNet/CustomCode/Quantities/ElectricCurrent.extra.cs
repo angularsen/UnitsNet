4e295d77679c45f1f31014fcd7068ae00b8bfb62
Rafael Gomez <rafitagd90@gmail.com>
2019-11-21 22:55:14 +0100

    Added operators for ohm's law

---
+++ UnitsNet/CustomCode/Quantities/ElectricCurrent.extra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitsNet
+{
+    public partial struct ElectricCurrent
+    {
+        /// <summary>Get <see cref="ElectricPotential"/> from <see cref="ElectricResistance"/> multiplied by <see cref="ElectricCurrent"/>.</summary>
+        /// <remarks>Ohm's law implementation</remarks>
+        public static ElectricPotential operator *(ElectricCurrent current, ElectricResistance resistance)
+        {
+            return ElectricPotential.FromVolts(resistance.Ohms * current.Amperes);
+        }
+    }
+}
