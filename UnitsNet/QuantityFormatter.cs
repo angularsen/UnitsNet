456153e7f518e7a40a2029ddd790e56e575a59e5
Tristan Milnthorp <tristan.milnthorp@ansys.com>
2020-05-22 09:39:19 -0400

    Add support for standard double numeric format strings. Also allow upper case

--- UnitsNet/QuantityFormatter.cs
+++ UnitsNet/QuantityFormatter.cs
@@ -14,6 +14,32 @@ namespace UnitsNet
     public class QuantityFormatter
     {
         /// <summary>
+        /// Formats the given quantity using the given format string. Uses the <see cref="CultureInfo.CurrentUICulture" />.
+        /// </summary>
+        /// <typeparam name="TUnitType">The quantity's unit type, for example <see cref="LengthUnit"/>.</typeparam>
+        /// <param name="quantity">The quantity to format.</param>
+        /// <param name="format">The format string.</param>
+        /// <remarks>
+        /// The valid format strings are as follows:
+        /// Any of the standard numeric format strings for double values except for "G" or "g" ("C" or "c", "E" or "e", "F" or "f", "N" or "n", "P" or "p", "R" or "r").
+        /// "G" or "g": The value with 2 significant digits after the radix followed by the unit abbreviation, such as "1.23 m".
+        /// "A" or "a": The default unit abbreviation for <see cref="IQuantity{TUnitType}.Unit" />, such as "m".
+        /// "a0", "a1", ..., "aN": The Nth unit abbreviation for <see cref="IQuantity{TUnitType}.Unit" />. "a0" is the same as "a".
+        /// A <see cref="FormatException"/> will be thrown if the requested abbreviation index does not exist.
+        /// "V" or "v": String representation of <see cref="IQuantity.Value" />.
+        /// "U" or "u": The enum name of <see cref="IQuantity{TUnitType}.Unit" />, such as "Meter".
+        /// "Q" or "q": The quantity name, such as "Length".
+        /// "s1", "s2", ..., "sN": The value with N significant digits after the radix followed by the unit abbreviation. For example,
+        /// "s4" would return "1.2345 m" if <see cref="IQuantity.Value" /> is 1.2345678. Trailing zeros are omitted.
+        /// </remarks>
+        /// <returns>The string representation.</returns>
+        public static string Format<TUnitType>(IQuantity<TUnitType> quantity, string format)
+            where TUnitType : Enum
+        {
+            return Format(quantity, format, CultureInfo.CurrentUICulture);
+        }
+
+        /// <summary>
         /// Formats the given quantity using the given format string and format provider.
         /// </summary>
         /// <typeparam name="TUnitType">The quantity's unit type, for example <see cref="LengthUnit"/>.</typeparam>
@@ -23,13 +49,14 @@ namespace UnitsNet
         /// <see cref="CultureInfo.CurrentUICulture" /> if null.</param>
         /// <remarks>
         /// The valid format strings are as follows:
-        /// "g": The value with 2 significant digits after the radix followed by the unit abbreviation, such as "1.23 m".
-        /// "a": The default unit abbreviation for <see cref="IQuantity{TUnitType}.Unit" />, such as "m".
+        /// Any of the standard numeric format strings for double values except for "G" or "g" ("C" or "c", "E" or "e", "F" or "f", "N" or "n", "P" or "p", "R" or "r").
+        /// "G" or "g": The value with 2 significant digits after the radix followed by the unit abbreviation, such as "1.23 m".
+        /// "A" or "a": The default unit abbreviation for <see cref="IQuantity{TUnitType}.Unit" />, such as "m".
         /// "a0", "a1", ..., "aN": The Nth unit abbreviation for <see cref="IQuantity{TUnitType}.Unit" />. "a0" is the same as "a".
         /// A <see cref="FormatException"/> will be thrown if the requested abbreviation index does not exist.
-        /// "v": String representation of <see cref="IQuantity.Value" />.
-        /// "u": The enum name of <see cref="IQuantity{TUnitType}.Unit" />, such as "Meter".
-        /// "q": The quantity name, such as "Length".
+        /// "V" or "v": String representation of <see cref="IQuantity.Value" />.
+        /// "U" or "u": The enum name of <see cref="IQuantity{TUnitType}.Unit" />, such as "Meter".
+        /// "Q" or "q": The quantity name, such as "Length".
         /// "s1", "s2", ..., "sN": The value with N significant digits after the radix followed by the unit abbreviation. For example,
         /// "s4" would return "1.2345 m" if <see cref="IQuantity.Value" /> is 1.2345678. Trailing zeros are omitted.
         /// </remarks>
@@ -37,41 +64,66 @@ namespace UnitsNet
         public static string Format<TUnitType>(IQuantity<TUnitType> quantity, string format, IFormatProvider formatProvider)
             where TUnitType : Enum
         {
-            formatProvider = formatProvider ?? CultureInfo.CurrentUICulture;
+            formatProvider ??= CultureInfo.CurrentUICulture;
 
-            var number = 0;
-            var formatString = format;
+            if(string.IsNullOrEmpty(format))
+                format = "g";
 
-            if(string.IsNullOrEmpty(formatString))
-                formatString = "g";
+            int precisionSpecifier = 0;
+            char formatSpecifier = format[0];
 
-            if(formatString.StartsWith("a") || formatString.StartsWith("s"))
+            switch(formatSpecifier)
             {
-                if(formatString.Length > 1 && !int.TryParse(formatString.Substring(1), out number))
-                    throw new FormatException($"The {format} format string is not supported.");
-
-                formatString = formatString.Substring(0, 1);
+                case 'A':
+                case 'a':
+                case 'S':
+                case 's':
+                    if(format.Length > 1 && !int.TryParse(format.Substring(1), out precisionSpecifier))
+                        throw new FormatException($"The {format} format string is not supported.");
+                    break;
             }
 
-            switch(formatString)
+            switch(formatSpecifier)
             {
-                case "g":
+                // Standard numeric format specifiers
+                case 'C':
+                case 'c':
+                case 'E':
+                case 'e':
+                case 'F':
+                case 'f':
+                case 'N':
+                case 'n':
+                case 'P':
+                case 'p':
+                case 'R':
+                case 'r':
+                    return quantity.Value.ToString(format, formatProvider);
+
+                // UnitsNet custom format specifiers
+                case 'G':
+                case 'g':
                     return ToStringWithSignificantDigitsAfterRadix(quantity, formatProvider, 2);
-                case "a":
+                case 'A':
+                case 'a':
                     var abbreviations = UnitAbbreviationsCache.Default.GetUnitAbbreviations(quantity.Unit, formatProvider);
 
-                    if(number >= abbreviations.Length)
+                    if(precisionSpecifier >= abbreviations.Length)
                         throw new FormatException($"The {format} format string is invalid because the abbreviation index does not exist.");
 
-                    return abbreviations[number];
-                case "v":
+                    return abbreviations[precisionSpecifier];
+                case 'V':
+                case 'v':
                     return quantity.Value.ToString(formatProvider);
-                case "u":
+                case 'U':
+                case 'u':
                     return quantity.Unit.ToString();
-                case "q":
+                case 'Q':
+                case 'q':
                     return quantity.QuantityInfo.Name;
-                case "s":
-                    return ToStringWithSignificantDigitsAfterRadix(quantity, formatProvider, number);
+                case 'S':
+                case 's':
+                    return ToStringWithSignificantDigitsAfterRadix(quantity, formatProvider, precisionSpecifier);
                 default:
                     throw new FormatException($"The {format} format string is not supported.");
             }
