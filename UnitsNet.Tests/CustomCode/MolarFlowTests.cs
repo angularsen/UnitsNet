344daff81c930f4c1c6f0c09133fa05b1b73fa8f
Alex Hogg <the.hoglet@gmail.com>
2021-01-29 19:01:28 +0000

    test pass

--- UnitsNet.Tests/CustomCode/MolarFlowTests.cs
+++ UnitsNet.Tests/CustomCode/MolarFlowTests.cs
@@ -23,29 +23,29 @@ namespace UnitsNet.Tests.CustomCode
 {
     public class MolarFlowTests : MolarFlowTestsBase
     {
-        protected override bool SupportsSIUnitSystem => true;
+        protected override bool SupportsSIUnitSystem => false;
 
-        protected override double CentimolesPerDayInOneMolePerSecond => 100d / (60d * 60d * 24d);
+        protected override double CentimolesPerDayInOneMolePerSecond => 100d * 60d * 60d * 24d;
         protected override double CentimolesPerSecondInOneMolePerSecond => 100d;
-        protected override double DecamolesPerDayInOneMolePerSecond => 0.1 / (60d * 60d * 24d);
+        protected override double DecamolesPerDayInOneMolePerSecond => 0.1 * 60d * 60d * 24d;
         protected override double DecamolesPerSecondInOneMolePerSecond => 0.1;
-        protected override double DecimolesPerDayInOneMolePerSecond => 10d / (60d * 60d * 24d);
+        protected override double DecimolesPerDayInOneMolePerSecond => 10d * 60d * 60d * 24d;
         protected override double DecimolesPerSecondInOneMolePerSecond => 10d;
-        protected override double HectomolesPerDayInOneMolePerSecond => 0.01 / (60d * 60d * 24d);
+        protected override double HectomolesPerDayInOneMolePerSecond => 0.01 * 60d * 60d * 24d;
         protected override double HectomolesPerSecondInOneMolePerSecond => 0.01;
-        protected override double KilomolesPerDayInOneMolePerSecond => 0.001 / (60d * 60d * 24d);
-        protected override double KilomolesPerHourInOneMolePerSecond => 0.001 / (60d * 60d);
-        protected override double KilomolesPerMinuteInOneMolePerSecond => 0.001 / 60d;
+        protected override double KilomolesPerDayInOneMolePerSecond => 0.001 * 60d * 60d * 24d;
+        protected override double KilomolesPerHourInOneMolePerSecond => 0.001 * 60d * 60d;
+        protected override double KilomolesPerMinuteInOneMolePerSecond => 0.001 * 60d;
         protected override double KilomolesPerSecondInOneMolePerSecond => 0.001;
-        protected override double MegamolesPerDayInOneMolePerSecond => 0.000_001 / (60d * 60d * 24d);
-        protected override double MicromolesPerDayInOneMolePerSecond => 1_000_000d / (60d * 60d * 24d);
+        protected override double MegamolesPerDayInOneMolePerSecond => 0.000_001 * 60d * 60d * 24d;
+        protected override double MicromolesPerDayInOneMolePerSecond => 1_000_000d * 60d * 60d * 24d;
         protected override double MicromolesPerSecondInOneMolePerSecond => 1_000_000d;
-        protected override double MillimolesPerDayInOneMolePerSecond => 1000d / (60d * 60d * 24d);
+        protected override double MillimolesPerDayInOneMolePerSecond => 1000d * 60d * 60d * 24d;
         protected override double MillimolesPerSecondInOneMolePerSecond => 1000d;
-        protected override double MolesPerDayInOneMolePerSecond => 1d / (60d * 60d * 24d);
-        protected override double MolesPerHourInOneMolePerSecond => 1d / (60d * 60d);
+        protected override double MolesPerDayInOneMolePerSecond => 1d * 60d * 60d * 24d;
+        protected override double MolesPerHourInOneMolePerSecond => 1d * 60d * 60d;
         protected override double MolesPerSecondInOneMolePerSecond => 1d;
-        protected override double NanomolesPerDayInOneMolePerSecond => 1_000_000_000d / (60d * 60d * 24d);
+        protected override double NanomolesPerDayInOneMolePerSecond => 1_000_000_000d * 60d * 60d * 24d;
         protected override double NanomolesPerSecondInOneMolePerSecond => 1_000_000_000d;
     }
 }
