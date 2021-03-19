2d23eac552d8760894bd467396e015c22e5e8245
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2021-03-19 23:53:21 +0100

    Revert "Trim ToString() for units without abbreviation"
    This reverts commit cbee20281e9a5470d1a4e3c19938d45700d611da.

--- UnitsNet/QuantityFormatter.cs
+++ UnitsNet/QuantityFormatter.cs
@@ -125,12 +125,6 @@ namespace UnitsNet
         public static string Format<TUnitType>(IQuantity<TUnitType> quantity, string format, IFormatProvider? formatProvider)
             where TUnitType : Enum
         {
-            return FormatUntrimmed(quantity, format, formatProvider).Trim();
-        }
-
-        private static string FormatUntrimmed<TUnitType>(IQuantity<TUnitType> quantity, string format, IFormatProvider? formatProvider)
-            where TUnitType : Enum
-        {
             formatProvider ??= CultureInfo.CurrentUICulture;
 
             if(string.IsNullOrWhiteSpace(format))
