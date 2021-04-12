b47d2a957a9cffe395a0da888650a4f457f41e1b
José Simões <jose.simoes@eclo.solutions>
2021-04-12 11:32:35 +0100

    Move all nanoFramework generated projects to its own folder
    - Move nfproj files.
    - Fix files path in all projects.
    - Fix references in all projects.
    - Fix project path in solution file.
    - Fix typo in solution name.

--- UnitsNet.NanoFramework/GeneratedCode/Properties/AssemblyInfo.cs
+++ UnitsNet.NanoFramework/GeneratedCode/Properties/AssemblyInfo.cs
@@ -51,8 +51,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-/////////////////////////////////////////////////////////////////
-// This attribute is mandatory when building Interop libraries //
-// update this whenever the native assembly signature changes  //
-[assembly: AssemblyNativeVersion("1.0.0.0")]
-/////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////
+// This assembly doens't require native support //
+[assembly: AssemblyNativeVersion("0.0.0.0")]
+//////////////////////////////////////////////////
