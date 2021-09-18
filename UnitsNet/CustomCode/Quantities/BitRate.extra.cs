cf739b1c20442fc40d7e7834e8a4e548050a0b7e
Breck Fresen <>
2021-09-18 00:37:53 -0500

    Overload * operator for BitRate and Duration
    This enables expressions of the form 1MB/sec * 1 hour = 3600MB.

---
+++ UnitsNet/CustomCode/Quantities/BitRate.extra.cs
@@ -0,0 +1,19 @@
+﻿// Licensed under MIT No Attribution, see LICENSE file at the root.
+
+namespace UnitsNet
+{
+    public partial struct BitRate
+    {
+        /// <summary>Get <see cref="Information"/> from <see cref="BitRate"/> times <see cref="Duration"/>.</summary>
+        public static Information operator *(BitRate bitRate, Duration duration)
+        {
+            return Information.FromBits(bitRate.BitsPerSecond * duration.Seconds);
+        }
+
+        /// <summary>Get <see cref="Information"/> from <see cref="Duration"/> times <see cref="BitRate"/>.</summary>
+        public static Information operator *(Duration duration, BitRate bitRate)
+        {
+            return Information.FromBits(bitRate.BitsPerSecond * duration.Seconds);
+        }
+    }
+}
