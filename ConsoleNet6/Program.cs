a7246b6f2a883e80cda09ebb450940ecd7e56b31
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2021-11-02 22:51:34 +0100

    Cleanup

--- ConsoleNet6/Program.cs
+++ ConsoleNet6/Program.cs
@@ -7,12 +7,10 @@ using static ConsoleNet6.LengthUnit;
 void Foo<TQuantity>(TQuantity left, TQuantity right)
     where TQuantity : IQuantity<TQuantity>
 {
-    WriteLine("Foo\n---");
-    WriteLine("left: " + left);
-    WriteLine("right: " + right);
-    WriteLine("left.CompareTo(right): "             + left.CompareTo(right));
-    WriteLine("left + right: "                      + (left + right));
-    WriteLine("TQuantity.Parse(\"25 m\", null): " + TQuantity.Parse("25 m", null));
+    WriteLine($"Foo({left}, {right})\n---\n");
+    WriteLine(left.CompareTo(right).ToString().PadRight(20)             + "<==  left.CompareTo(right)");
+    WriteLine((left + right).ToString()!.PadRight(20)                   + "<==  left + right");
+    WriteLine(TQuantity.Parse("25 m", null).ToString()!.PadRight(20)    + "<==  TQuantity.Parse(\"25 m\", null)");
 }
 
 TQuantity Sum<TQuantity>(IEnumerable<TQuantity> items)
@@ -24,9 +22,7 @@ TQuantity Sum<TQuantity>(IEnumerable<TQuantity> items)
 var oneMeter = new Length(1, Meter);
 var tenCentimeters = new Length(10, Centimeter);
 
-WriteLine("oneMeter + tenCentimeters: " + (oneMeter + tenCentimeters));
-
-Foo(new Length(1, Meter), new Length(10, Centimeter));
+Foo(oneMeter, tenCentimeters);
 
 Length[] lengths = new[] { 1, 2, 3, 4 }.Select(val => new Length(val, Meter)).ToArray();
-WriteLine("Sum [1,2,3,4] array of meters: " + Sum(lengths));
+WriteLine(Sum(lengths).ToString()!.PadRight(20)                          + "<==  Sum [1,2,3,4] array of meters");
