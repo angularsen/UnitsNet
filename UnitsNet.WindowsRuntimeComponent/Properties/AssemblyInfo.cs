2c3cb93cffd453a296a64ed0eadc5d02e35a4836
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2022-01-18 00:16:43 +0100

    UnitsNet: 4.114.0

--- UnitsNet.WindowsRuntimeComponent/Properties/AssemblyInfo.cs
+++ UnitsNet.WindowsRuntimeComponent/Properties/AssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("")]
-[assembly: AssemblyVersion("4.113.0")]
-[assembly: AssemblyFileVersion("4.113.0")]
+[assembly: AssemblyVersion("4.114.0")]
+[assembly: AssemblyFileVersion("4.114.0")]
 [assembly: InternalsVisibleTo("UnitsNet.WindowsRuntimeComponent.Tests")]
