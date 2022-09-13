6e7ba1c20bd50fff0956b96a161f1b163d221b81
Tilman Reinhardt <tilman.reinhardt@arup.com>
2022-09-13 15:54:51 +0200

    oasys tests

---
+++ UnitsNet.Tests/Oasys/UtilityTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using UnitsNet.Units;
+using Xunit;
+
+namespace UnitsNet.Tests.Oasys
+{
+    public class UtilityTests
+    {
+        [Fact]
+        public void OasysUnitsHaveAbbreviation()
+        {
+            Assert.Equal("lbf", AxialStiffness.GetAbbreviation(AxialStiffnessUnit.PoundForce));
+            Assert.Equal("lbf·ft²", BendingStiffness.GetAbbreviation(BendingStiffnessUnit.PoundForceSquareFoot));
+            Assert.Equal("m⁻¹", Curvature.GetAbbreviation(CurvatureUnit.PerMeter));
+            Assert.Equal("MN·m", Moment.GetAbbreviation(MomentUnit.MeganewtonMeter));
+            Assert.Equal("m³", SectionModulus.GetAbbreviation(SectionModulusUnit.CubicMeter));
+            Assert.Equal("µε", Strain.GetAbbreviation(StrainUnit.MicroStrain));
+        }
+
+        [Fact]
+        public void GetAbbreviationReturnsStringInUSEnglishIfAbbreviationIsNotFound()
+        {
+            Assert.Equal("lbf", AxialStiffness.GetAbbreviation(AxialStiffnessUnit.PoundForce, new CultureInfo("zh-CN")));
+            Assert.Equal("lbf·ft²", BendingStiffness.GetAbbreviation(BendingStiffnessUnit.PoundForceSquareFoot, new CultureInfo("zh-CN")));
+            Assert.Equal("m⁻¹", Curvature.GetAbbreviation(CurvatureUnit.PerMeter, new CultureInfo("zh-CN")));
+            Assert.Equal("MN·m", Moment.GetAbbreviation(MomentUnit.MeganewtonMeter, new CultureInfo("zh-CN")));
+            Assert.Equal("m³", SectionModulus.GetAbbreviation(SectionModulusUnit.CubicMeter, new CultureInfo("zh-CN")));
+            Assert.Equal("µε", Strain.GetAbbreviation(StrainUnit.MicroStrain, new CultureInfo("zh-CN")));
+        }
+    }
+}
