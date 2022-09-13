8a04b3f064112b7e0c7f30514e5f4a91cb92d813
Tilman Reinhardt <tilman.reinhardt@arup.com>
2022-09-13 16:41:06 +0200

    oasys tests

--- UnitsNet.Tests/Oasys/AbbreviationTests.cs
+++ UnitsNet.Tests/Oasys/AbbreviationTests.cs
@@ -4,10 +4,18 @@ using Xunit;
 
 namespace UnitsNet.Tests.Oasys
 {
-    public class UtilityTests
+    public class AbbreviationTests
     {
         [Fact]
-        public void OasysUnitsHaveAbbreviation()
+        public void GetAbbreviationFromToStringTest()
+        {
+            Moment moment = new Moment(1, MomentUnit.KilonewtonMeter);
+            string unitAbbreviation = moment.ToString("a");
+            Assert.Equal("kN·m", unitAbbreviation);
+        }
+
+        [Fact]
+        public void OasysUnitsHaveAbbreviationTest()
         {
             Assert.Equal("lbf", AxialStiffness.GetAbbreviation(AxialStiffnessUnit.PoundForce));
             Assert.Equal("lbf·ft²", BendingStiffness.GetAbbreviation(BendingStiffnessUnit.PoundForceSquareFoot));
@@ -18,7 +26,7 @@ namespace UnitsNet.Tests.Oasys
         }
 
         [Fact]
-        public void GetAbbreviationReturnsStringInUSEnglishIfAbbreviationIsNotFound()
+        public void GetAbbreviationReturnsStringInUSEnglishIfAbbreviationIsNotFoundTest()
         {
             Assert.Equal("lbf", AxialStiffness.GetAbbreviation(AxialStiffnessUnit.PoundForce, new CultureInfo("zh-CN")));
             Assert.Equal("lbf·ft²", BendingStiffness.GetAbbreviation(BendingStiffnessUnit.PoundForceSquareFoot, new CultureInfo("zh-CN")));
@@ -26,6 +34,25 @@ namespace UnitsNet.Tests.Oasys
             Assert.Equal("MN·m", Moment.GetAbbreviation(MomentUnit.MeganewtonMeter, new CultureInfo("zh-CN")));
             Assert.Equal("m³", SectionModulus.GetAbbreviation(SectionModulusUnit.CubicMeter, new CultureInfo("zh-CN")));
             Assert.Equal("µε", Strain.GetAbbreviation(StrainUnit.MicroStrain, new CultureInfo("zh-CN")));
+        }
+
+        [Fact]
+        public void OasysUnitsParseAbbreviationTest()
+        {
+            Assert.Equal(AxialStiffnessUnit.PoundForce, AxialStiffness.ParseUnit("lbf"));
+            Assert.Equal(BendingStiffnessUnit.PoundForceSquareFoot, BendingStiffness.ParseUnit("lbf·ft²"));
+            Assert.Equal(CurvatureUnit.PerMeter, Curvature.ParseUnit("m⁻¹"));
+            Assert.Equal(MomentUnit.MeganewtonMeter, Moment.ParseUnit("MN·m"));
+            Assert.Equal(SectionModulusUnit.CubicMeter, SectionModulus.ParseUnit("m³"));
+            Assert.Equal(StrainUnit.MicroStrain, Strain.ParseUnit("µε"));
+        }
+
+        [Fact]
+        public void ParseMomentUnitFromAbbreviation()
+        {
+            string unitAbbreviation = Moment.GetAbbreviation(MomentUnit.KilonewtonMeter);
+            MomentUnit momentUnit = Moment.ParseUnit(unitAbbreviation);
+            Assert.Equal(MomentUnit.KilonewtonMeter, momentUnit);
         }
     }
 }
---
+++ UnitsNet.Tests/Oasys/QuantityTryFromTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using UnitsNet.Units;
+using Xunit;
+
+namespace UnitsNet.Tests.Oasys
+{
+    public class QuantityTryFromTests
+    {
+        [Fact]
+        public void MomentUnitTryFromTest()
+        {
+            double val = 2;
+            MomentUnit unit = MomentUnit.KilopoundForceFoot;
+            Assert.True(Quantity.TryFrom(val, unit, out IQuantity quantity));
+            Assert.Equal(val, quantity.As(unit));
+        }
+    }
+}
