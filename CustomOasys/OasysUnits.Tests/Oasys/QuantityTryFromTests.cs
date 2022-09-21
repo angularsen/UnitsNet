8e380c40dcb762cc820673860096196f5d6f92c6
Tilman Reinhardt <tilman.reinhardt@arup.com>
2022-09-21 17:14:42 +0200

    added folder with custom oasys files

---
+++ CustomOasys/OasysUnits.Tests/Oasys/QuantityTryFromTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using OasysUnits.Units;
+using Xunit;
+
+namespace OasysUnits.Tests.Oasys
+{
+    public class QuantityTryFromTests
+    {
+        [Fact]
+        public void MomentUnitTryFromTest()
+        {
+            double val = 2;
+            MomentUnit unit = MomentUnit.KilopoundForceFoot;
+            Assert.True(Quantity.TryFrom(val, unit, out IQuantity quantity));
+            Assert.Equal(val, quantity.As(unit));
+        }
+    }
+}
---
+++ CustomOasys/OasysUnits/CustomCode/Quantities/Strain.extra.cs
@@ -0,0 +1,14 @@
+﻿// Licensed under MIT No Attribution, see LICENSE file at the root.
+// Copyright 2013 Andreas Gullberg Larsen (andreas.larsen84@gmail.com). Maintained at https://github.com/angularsen/UnitsNet.
+
+namespace OasysUnits
+{
+    public partial struct Strain
+    {
+        /// <summary>Get <see cref="AxialStiffness"/> from <see cref="Force"/> divided by <see cref="Strain"/>.</summary>
+        public static AxialStiffness operator /(Force force, Strain strain)
+        {
+            return AxialStiffness.FromNewtons(force.Newtons / strain.Ratio);
+        }
+    }
+}
