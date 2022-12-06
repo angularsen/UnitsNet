e6886482d7e9d48666bafb577a5a864ccfa618e6
kasperk81 <83082615+kasperk81@users.noreply.github.com>
2022-12-06 03:30:41 +0000

    update benchmarks to net6.0

--- UnitsNet.Benchmark/Scripts/json-export-net5.bat
+++ UnitsNet.Benchmark/Scripts/json-export-net5.bat
@@ -5,7 +5,7 @@ SET exportdir="%projectdir%\Artifacts\Benchmark"
 :: this fails on the build server (also tested with the nightly benchmark.net package: 0.12.1.1533): possibly related to https://github.com/dotnet/BenchmarkDotNet/issues/1487
 dotnet run --project "%projectdir%\UnitsNet.Benchmark" -c Release ^
 --framework net5.0 ^
---runtimes netcoreapp50 ^
+--runtimes net5.0 ^
 --artifacts=%exportdir% ^
 --exporters json ^
 --filter * ^
@@ -14,4 +14,4 @@ dotnet run --project "%projectdir%\UnitsNet.Benchmark" -c Release ^
 --join %1 %2 %3
 
 :: this runs fine, however there is currently no way of displaying multiple-lines-per-chart: see https://github.com/rhysd/github-action-benchmark/issues/18
-:: dotnet run --project "%scriptdir%/UnitsNet.Benchmark" -c Release -f net5.0 --runtimes netcoreapp31 netcoreapp50 --filter ** --artifacts="%scriptdir%/Artifacts/Benchmark" --exporters json
+:: dotnet run --project "%scriptdir%/UnitsNet.Benchmark" -c Release -f net5.0 --runtimes netcoreapp31 net5.0 --filter ** --artifacts="%scriptdir%/Artifacts/Benchmark" --exporters json
