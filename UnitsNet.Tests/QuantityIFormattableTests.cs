ab1f41fc9e7a8b82b38502bfe8ea0fa507350a0d
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2022-12-26 20:32:59 +0100

    ♻️ Remove unused usings

--- UnitsNet.Tests/QuantityIFormattableTests.cs
+++ UnitsNet.Tests/QuantityIFormattableTests.cs
@@ -2,7 +2,6 @@
 // Copyright 2013 Andreas Gullberg Larsen (andreas.larsen84@gmail.com). Maintained at https://github.com/angularsen/UnitsNet.
 
 using System;
-using System.Globalization;
 using Xunit;
 
 namespace UnitsNet.Tests
