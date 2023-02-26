7a5eabac52b1b915fbf8107c0b09ddf9e6265c3d
Tristan Milnthorp <tmilnthorp@gmail.com>
2023-02-26 15:24:14 -0500

    Remove versioning scripts

--- publish-unitsnet.bat
+++ publish-unitsnet.bat
@@ -5,9 +5,6 @@ if %errorlevel% neq 0 goto EXIT
 call git pull --rebase --prune
 if %errorlevel% neq 0 goto EXIT
 
-call build\bump-version-UnitsNet-minor.bat
-if %errorlevel% neq 0 goto EXIT
-
 call git push --follow-tags --set-upstream
 if %errorlevel% neq 0 goto EXIT
 
