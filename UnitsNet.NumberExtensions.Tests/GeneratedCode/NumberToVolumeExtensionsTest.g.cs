3bd016ab5edbc90c44b708f4161be667c5ae5b59
Conrad Heidebrecht <conrad@hh-dev.com>
2023-06-02 08:55:44 -0400

    Add ImperialQuart to Volume (#1254)

--- UnitsNet.NumberExtensions.Tests/GeneratedCode/NumberToVolumeExtensionsTest.g.cs
+++ UnitsNet.NumberExtensions.Tests/GeneratedCode/NumberToVolumeExtensionsTest.g.cs
@@ -133,6 +133,10 @@ namespace UnitsNet.Tests
             Assert.Equal(Volume.FromImperialPints(2), 2.ImperialPints());
 
         [Fact]
+        public void NumberToImperialQuartsTest() =>
+            Assert.Equal(Volume.FromImperialQuarts(2), 2.ImperialQuarts());
+
+        [Fact]
         public void NumberToKilocubicFeetTest() =>
             Assert.Equal(Volume.FromKilocubicFeet(2), 2.KilocubicFeet());
 
