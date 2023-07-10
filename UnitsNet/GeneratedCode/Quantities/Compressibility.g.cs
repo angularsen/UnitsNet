07e9f72497499263de9a4eb92ce5dcd10b626aba
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2023-07-10 23:57:44 +0200

    ✏️Fix whitespace in codegen (#1282)

--- UnitsNet/GeneratedCode/Quantities/Compressibility.g.cs
+++ UnitsNet/GeneratedCode/Quantities/Compressibility.g.cs
@@ -33,7 +33,7 @@ namespace UnitsNet
 {
     /// <inheritdoc />
     /// <summary>
-    ///
+    ///     
     /// </summary>
     [DataContract]
     public readonly partial struct Compressibility :
