7afdd82fadd53578f034edcc40471eebc1540e70
Patrick Liebe <107194118+pliebe91@users.noreply.github.com>
2023-09-21 08:09:19 +0200

    Add micro, nano prefix to Energy unit Joule (#1316)
    Hello,
    
    For the correct display of the values of our laser energy meters we are
    currently missing the prefixes micro and nano for the unit joule.
    
    It would be nice if this could be added.
    
    Many thanks

--- UnitsNet.NumberExtensions.Tests/GeneratedCode/NumberToEnergyExtensionsTest.g.cs
+++ UnitsNet.NumberExtensions.Tests/GeneratedCode/NumberToEnergyExtensionsTest.g.cs
@@ -133,8 +133,16 @@ namespace UnitsNet.Tests
             Assert.Equal(Energy.FromMegawattHours(2), 2.MegawattHours());
 
         [Fact]
+        public void NumberToMicrojoulesTest() =>
+            Assert.Equal(Energy.FromMicrojoules(2), 2.Microjoules());
+
+        [Fact]
         public void NumberToMillijoulesTest() =>
             Assert.Equal(Energy.FromMillijoules(2), 2.Millijoules());
+
+        [Fact]
+        public void NumberToNanojoulesTest() =>
+            Assert.Equal(Energy.FromNanojoules(2), 2.Nanojoules());
 
         [Fact]
         public void NumberToPetajoulesTest() =>
