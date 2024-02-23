41596032936c763286ce17c6ac952cd12dce4b3a
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2024-02-24 00:04:57 +0100

    UnitsNet: 5.47.0

--- CodeGen/Generators/NanoFrameworkGen/NuspecGenerator.cs
+++ CodeGen/Generators/NanoFrameworkGen/NuspecGenerator.cs
@@ -25,7 +25,7 @@ namespace CodeGen.Generators.NanoFrameworkGen
 <package xmlns=""http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd"">
   <metadata>
     <id>UnitsNet.nanoFramework.{_quantity.Name}</id>
-    <version>5.46.0-pre</version>
+    <version>5.47.0</version>
     <title>Units.NET {_quantity.Name} - nanoFramework</title>
     <authors>Andreas Gullberg Larsen,nanoframework</authors>
     <owners>UnitsNet</owners>
