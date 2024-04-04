b9b4365313ddc598caa0b9ade6b1c2d4250c3e4d
Muximize <Muximize@users.noreply.github.com>
2024-04-04 18:24:11 +0200

    Update Nuget dependencies (#1384)

--- UnitsNet.Tests/CustomCode/ParseTests.cs
+++ UnitsNet.Tests/CustomCode/ParseTests.cs
@@ -39,10 +39,10 @@ namespace UnitsNet.Tests
         [InlineData("1 kg", typeof(FormatException))] // Wrong measurement type.
         [InlineData("1ft monkey 1in", typeof(FormatException))] // Invalid separator between two valid measurements.
         [InlineData("1ft 1invalid", typeof(FormatException))] // Valid
-        public void ParseLength_InvalidString_USEnglish_ThrowsException(string s, Type expectedExceptionType)
+        public void ParseLength_InvalidString_USEnglish_ThrowsException(string? s, Type expectedExceptionType)
         {
             var usEnglish = CultureInfo.GetCultureInfo("en-US");
-            Assert.Throws(expectedExceptionType, () => Length.Parse(s, usEnglish));
+            Assert.Throws(expectedExceptionType, () => Length.Parse(s!, usEnglish));
         }
 
         /// <exception cref="UnitsNetException">Error parsing string.</exception>
@@ -50,7 +50,7 @@ namespace UnitsNet.Tests
         [InlineData("5.5 m", 5.5)]
         [InlineData("500 005 m", 500005)]
         // quantity doesn't match number format
-        public void ParseWithCultureUsingSpaceAsThousandSeparators(string s, double expected)
+        public void ParseWithCultureUsingSpaceAsThousandSeparators(string? s, double expected)
         {
             var numberFormat = (NumberFormatInfo) CultureInfo.InvariantCulture.NumberFormat.Clone();
             numberFormat.NumberGroupSeparator = " ";
@@ -58,7 +58,7 @@ namespace UnitsNet.Tests
             numberFormat.NumberDecimalSeparator = ".";
             numberFormat.CurrencyDecimalSeparator = ".";
 
-            double actual = Length.Parse(s, numberFormat).Meters;
+            double actual = Length.Parse(s!, numberFormat).Meters;
             Assert.Equal(expected, actual);
         }
 
@@ -125,10 +125,10 @@ namespace UnitsNet.Tests
         [Theory]
         [InlineData("kg", typeof(UnitNotFoundException))]
         [InlineData(null, typeof(ArgumentNullException))]
-        public void ParseLengthUnitUsEnglish_ThrowsExceptionOnInvalidString(string s, Type expectedExceptionType)
+        public void ParseLengthUnitUsEnglish_ThrowsExceptionOnInvalidString(string? s, Type expectedExceptionType)
         {
             var usEnglish = CultureInfo.GetCultureInfo("en-US");
-            Assert.Throws(expectedExceptionType, () => Length.ParseUnit(s, usEnglish));
+            Assert.Throws(expectedExceptionType, () => Length.ParseUnit(s!, usEnglish));
         }
 
         [Theory]
@@ -137,7 +137,7 @@ namespace UnitsNet.Tests
         [InlineData("2 kg", false)]
         [InlineData(null, false)]
         [InlineData("foo", false)]
-        public void TryParseLengthUnitUsEnglish(string s, bool expected)
+        public void TryParseLengthUnitUsEnglish(string? s, bool expected)
         {
             CultureInfo usEnglish = CultureInfo.GetCultureInfo("en-US");
             bool actual = Length.TryParse(s, usEnglish, out Length _);
