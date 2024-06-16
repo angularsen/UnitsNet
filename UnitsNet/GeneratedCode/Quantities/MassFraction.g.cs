7053fbe578d6ee44f000ea9dda698318b3f5840b
lipchev <lipchev@gmail.com>
2024-06-16 14:22:46 +0300

    fixes #1394 : QuantityGenerator ignores ComparisonType in the obsolete Equals method

--- UnitsNet/GeneratedCode/Quantities/MassFraction.g.cs
+++ UnitsNet/GeneratedCode/Quantities/MassFraction.g.cs
@@ -1000,7 +1000,7 @@ namespace UnitsNet
                 referenceValue: this.Value,
                 otherValue: other.As(this.Unit),
                 tolerance: tolerance,
-                comparisonType: ComparisonType.Absolute);
+                comparisonType: comparisonType);
         }
 
         /// <inheritdoc />
