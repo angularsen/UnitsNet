1e9a3e67631981269d83a081bcab28945bab7cc7
Burak Can <bc913dev@outlook.com>
2024-06-20 14:16:06 -0400

    Remove unused using statements.

--- UnitsNet.Tests/CustomCode/ElectricConductancePerAreaTests.cs
+++ UnitsNet.Tests/CustomCode/ElectricConductancePerAreaTests.cs
@@ -17,7 +17,6 @@
 // Licensed under MIT No Attribution, see LICENSE file at the root.
 // Copyright 2013 Andreas Gullberg Larsen (andreas.larsen84@gmail.com). Maintained at https://github.com/angularsen/UnitsNet.
 
-using UnitsNet.Units;
 using Xunit;
 
 namespace UnitsNet.Tests.CustomCode
