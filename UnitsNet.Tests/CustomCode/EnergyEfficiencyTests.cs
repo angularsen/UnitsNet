9d940d2febec901c013615c25dfa8d221bcfd62f
Chef904 <johannes-t@t-online.de>
2024-08-29 15:30:46 +0200

    chore: Update EnergyEfficiencyTests with unit conversions

--- UnitsNet.Tests/CustomCode/EnergyEfficiencyTests.cs
+++ UnitsNet.Tests/CustomCode/EnergyEfficiencyTests.cs
@@ -23,6 +23,23 @@ namespace UnitsNet.Tests.CustomCode
 {
     public class EnergyEfficiencyTests : EnergyEfficiencyTestsBase
     {
-        // Override properties in base class here
+        protected override double FemtowattHoursPerKilometerInOneWattHourPerKilometer => 1e15;
+        protected override double FemtowattHoursPerMeterInOneWattHourPerKilometer  => 1e12;
+        protected override double GigawattHoursPerKilometerInOneWattHourPerKilometer  => 1e-9;
+        protected override double GigawattHoursPerMeterInOneWattHourPerKilometer  => 1e-12;
+        protected override double KilowattHoursPerKilometerInOneWattHourPerKilometer  => 1e-3;
+        protected override double KilowattHoursPerMeterInOneWattHourPerKilometer  => 1e-6;
+        protected override double MegawattHoursPerKilometerInOneWattHourPerKilometer  => 1e-6;
+        protected override double MegawattHoursPerMeterInOneWattHourPerKilometer  => 1e-9;
+        protected override double MicrowattHoursPerKilometerInOneWattHourPerKilometer  => 1e6;
+        protected override double MicrowattHoursPerMeterInOneWattHourPerKilometer  => 1e3;
+        protected override double MilliwattHoursPerKilometerInOneWattHourPerKilometer  => 1e3;
+        protected override double MilliwattHoursPerMeterInOneWattHourPerKilometer  => 1;
+        protected override double NanowattHoursPerKilometerInOneWattHourPerKilometer  => 1e9;
+        protected override double NanowattHoursPerMeterInOneWattHourPerKilometer  => 1e6;
+        protected override double PicowattHoursPerKilometerInOneWattHourPerKilometer => 1e12;
+        protected override double PicowattHoursPerMeterInOneWattHourPerKilometer  => 1e9;
+        protected override double WattHoursPerKilometerInOneWattHourPerKilometer  => 1;
+        protected override double WattHoursPerMeterInOneWattHourPerKilometer  => 1e-3;
     }
 }
