81ed84cd769630690dab962d03483fb72672c6b5
lipchev <lipchev@gmail.com>
2024-12-11 09:43:57 +0200

    add a few more benchmarks

---
+++ UnitsNet.Benchmark/Comparisons/ComparisonBenchmarks.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace UnitsNet.Benchmark.Comparisons;
+
+[MemoryDiagnoser]
+[ShortRunJob(RuntimeMoniker.Net48)]
+[ShortRunJob(RuntimeMoniker.Net80)]
+public class ComparisonBenchmarks
+{
+    private static readonly Mass Tolerance = Mass.FromNanograms(1);
+
+    public static IEnumerable<object[]> Operands()
+    {
+        // equal value and unit
+        yield return [Mass.From(42, Mass.BaseUnit), Mass.From(42, Mass.BaseUnit)];
+        // equal value and unit
+        yield return [Mass.FromGrams(42), Mass.FromGrams(42)];
+        // zero in another unit
+        yield return [Mass.Zero, Mass.FromGrams(0)];
+        // same quantity in another unit
+        yield return [Mass.FromGrams(42), Mass.FromMilligrams(42000)];
+        // same quantity in another unit (in reverse)
+        yield return [Mass.FromMilligrams(42000), Mass.FromGrams(42)];
+        // different value and same unit
+        yield return [Mass.FromGrams(42), Mass.FromGrams(42.1)];
+        // huge values, same unit
+        yield return [Mass.FromGrams(-1e37), Mass.FromGrams(1 / 1e12)];
+        // huge values, different units
+        yield return [Mass.FromGrams(-1e37), Mass.FromMilligrams(1 / 1e12)];
+        // Math.PI, same unit
+        yield return [Mass.FromGrams(Math.PI), Mass.FromGrams(Math.PI)];
+        // Math.PI, different units
+        yield return [Mass.FromGrams(Math.PI), Mass.FromMilligrams(Math.PI)];
+        // very close fractions, same units
+        yield return [Mass.FromGrams(12.3456789987654321), Mass.FromGrams(12.3456789987654322)];
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Operands))]
+    public bool Equals(Mass a, Mass b)
+    {
+        return a.Equals(b);
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Operands))]
+    public bool EqualsTolerance(Mass a, Mass b)
+    {
+        return a.Equals(b, Tolerance);
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Operands))]
+    public bool GetHashCode(Mass a, Mass b)
+    {
+        return a.GetHashCode() == b.GetHashCode();
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Operands))]
+    public int CompareTo(Mass a, Mass b)
+    {
+        return a.CompareTo(b);
+    }
+}
---
+++ UnitsNet.Benchmark/Operators/Additions/SumOfVolumesWithRandomUnitsBenchmarks.cs
@@ -0,0 +1,60 @@
+﻿// Licensed under MIT No Attribution, see LICENSE file at the root.
+// Copyright 2013 Andreas Gullberg Larsen (andreas.larsen84@gmail.com). Maintained at https://github.com/angularsen/UnitsNet.
+
+using System;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using UnitsNet.Units;
+
+namespace UnitsNet.Benchmark.Operators.Additions;
+
+[MemoryDiagnoser]
+[SimpleJob(RuntimeMoniker.Net48)]
+[SimpleJob(RuntimeMoniker.Net90)]
+public class SumOfVolumesWithRandomUnitsBenchmarks
+{
+    private static readonly double Value = 1.23;
+
+    private readonly Random _random = new(41);
+    private Volume[] _quantities;
+
+    [Params(10, 100, 1000)]
+    public int NbOperations { get; set; }
+
+    [GlobalSetup]
+    public void PrepareQuantities()
+    {
+        _quantities = _random.GetRandomQuantities<Volume, VolumeUnit>(Value, Volume.Units, NbOperations).ToArray();
+        Quantity.From(Value, Volume.BaseUnit); // TODO we need a better way to "disable" the lazy loading of the _quantitiesByUnitType (QuantityInfoLookup)
+        Console.Out.WriteLine("Quantities prepared: starting unit = {0}", _quantities[0].Unit);
+    }
+    
+    [Benchmark(Baseline = true)]
+    public Volume SumOfVolumes()
+    {
+#if NET
+        return UnitsNet.GenericMath.GenericMathExtensions.Sum(_quantities);
+#else
+        Volume sum = Volume.Zero;
+        foreach (var quantity in _quantities)
+        {
+            sum = quantity + sum;
+        }
+
+        return sum;
+#endif
+    }
+    
+    [Benchmark(Baseline = false)]
+    public Volume SumOfVolumesInBaseUnit()
+    {
+        return _quantities.Sum(Volume.BaseUnit);
+    }
+    
+    [Benchmark(Baseline = false)]
+    public Volume SumOfVolumesInMilliliter()
+    {
+        return _quantities.Sum(VolumeUnit.Milliliter);
+    }
+}
