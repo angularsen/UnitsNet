angularsen/UnitsNet

commit 6b05cc8512df55681ffc3939bbfbb9d7a9465c3e

Add/rename electric quantities (#1444)
Renamed quantities to have prefix "Electric", marking old name as
obsolete and adding new duplicate quantities:
* `ApparentEnergy` -> `ElectricApparentEnergy`
* `ApparentPower` -> `ElectricApparentPower`
* `Capacitance` -> `ElectricCapacitance`
* `ReactiveEnergy` -> `ElectricReactiveEnergy`
* `ReactivePower` -> `ElectricReactivePower `
* `ElectricPotentialAc`
* `ElectricPotentialDc`
* `ElectricAdmittance` (obsolete due to being a complex number, not
supported by UnitsNet, recommending `ElectricReactance` and
`ElectricSusceptance` instead)
 
Decided against adding `ElectricImpedance`, also being a complex number.

New:
- Add `ElectricReactance`
- Add `ElectricSusceptance`
- Add `EletricCurrent = ElectricApparentPower / ElectricPotential`
- Add `ElectricPotential = ElectricApparentPower / ElectricCurrent`
- `ElectricAdmittance`: 
  - Add prefixes: Kilo, Mega, Giga, Tera
  - Add Mho unit, equivalent to Siemen
- `ElectricConductance`:
  - Add prefixes: Mega, Giga, Tera
  - Add Mho unit, equivalent to Siemen
- `EletricResistance`:
  - Add prefixes: Nano

---------

Co-authored-by: Andreas Gullberg Larsen <andreas.larsen84@gmail.com>

UnitsNet.NanoFramework/GeneratedCode/Quantities/ApparentEnergy.g.cs
@@ -26,6 +26,10 @@ namespace UnitsNet
     /// <summary>
     ///     A unit for expressing the integral of apparent power over time, equal to the product of 1 volt-ampere and 1 hour, or to 3600 joules.
     /// </summary>
+    /// <remarks>
+    ///     <see cref="ApparentEnergy" /> has been renamed to <see cref="ElectricApparentEnergy" />, and will be removed in a later major version.
+    /// </remarks>
+    [Obsolete("ApparentEnergy has been renamed to ElectricApparentEnergy, and will be removed in a later major version.")]
     public struct  ApparentEnergy
     {
         /// <summary>

UnitsNet.NanoFramework/GeneratedCode/Quantities/ApparentPower.g.cs
@@ -26,6 +26,10 @@ namespace UnitsNet
     /// <summary>
     ///     Power engineers measure apparent power as the magnitude of the vector sum of active and reactive power. Apparent power is the product of the root-mean-square of voltage and current.
     /// </summary>
+    /// <remarks>
+    ///     <see cref="ApparentPower" /> has been renamed to <see cref="ElectricApparentPower" />, and will be removed in a later major version.
+    /// </remarks>
+    [Obsolete("ApparentPower has been renamed to ElectricApparentPower, and will be removed in a later major version.")]
     public struct  ApparentPower
     {
         /// <summary>

UnitsNet.NanoFramework/GeneratedCode/Quantities/Capacitance.g.cs
@@ -27,8 +27,9 @@ namespace UnitsNet
     ///     Capacitance is the ability of a body to store an electric charge.
     /// </summary>
     /// <remarks>
-    ///     https://en.wikipedia.org/wiki/Capacitance
+    ///     <see cref="Capacitance" /> has been renamed to <see cref="ElectricCapacitance" />, and will be removed in a later major version.
     /// </remarks>
+    [Obsolete("Capacitance has been renamed to ElectricCapacitance, and will be removed in a later major version.")]
     public struct  Capacitance
     {
         /// <summary>

UnitsNet.NanoFramework/GeneratedCode/Quantities/ElectricCurrent.g.cs
@@ -26,6 +26,9 @@ namespace UnitsNet
     /// <summary>
     ///     An electric current is a flow of electric charge. In electric circuits this charge is often carried by moving electrons in a wire. It can also be carried by ions in an electrolyte, or by both ions and electrons such as in a plasma.
     /// </summary>
+    /// <remarks>
+    ///     If you want to map more parameters into the <see cref="ElectricCurrent" /> class (amps RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
+    /// </remarks>
     public struct  ElectricCurrent
     {
         /// <summary>

UnitsNet.NanoFramework/GeneratedCode/Quantities/ElectricPotential.g.cs
@@ -26,6 +26,9 @@ namespace UnitsNet
     /// <summary>
     ///     In classical electromagnetism, the electric potential (a scalar quantity denoted by Φ, ΦE or V and also called the electric field potential or the electrostatic potential) at a point is the amount of electric potential energy that a unitary point charge would have when located at that point.
     /// </summary>
+    /// <remarks>
+    ///     If you want to map more parameters into the <see cref="ElectricPotential" /> class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
+    /// </remarks>
     public struct  ElectricPotential
     {
         /// <summary>

UnitsNet.NanoFramework/GeneratedCode/Quantities/ElectricPotentialAc.g.cs
@@ -26,6 +26,10 @@ namespace UnitsNet
     /// <summary>
     ///     The Electric Potential of a system known to use Alternating Current.
     /// </summary>
+    /// <remarks>
+    ///     <see cref="ElectricPotentialAc" /> has been merged into <see cref="ElectricPotential" />, and will be removed in a later major version. If you want to map more parameters into the <see cref="ElectricPotential" /> class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
+    /// </remarks>
+    [Obsolete("ElectricPotentialAc has been merged into ElectricPotential, and will be removed in a later major version. If you want to map more parameters into the ElectricPotential class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.")]
     public struct  ElectricPotentialAc
     {
         /// <summary>

UnitsNet.NanoFramework/GeneratedCode/Quantities/ElectricPotentialDc.g.cs
@@ -26,6 +26,10 @@ namespace UnitsNet
     /// <summary>
     ///     The Electric Potential of a system known to use Direct Current.
     /// </summary>
+    /// <remarks>
+    ///     <see cref="ElectricPotentialDc" /> has been merged into <see cref="ElectricPotential" />, and will be removed in a later major version. If you want to map more parameters into the <see cref="ElectricPotential" /> class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
+    /// </remarks>
+    [Obsolete("ElectricPotentialDc has been merged into ElectricPotential, and will be removed in a later major version. If you want to map more parameters into the ElectricPotential class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.")]
     public struct  ElectricPotentialDc
     {
         /// <summary>

UnitsNet.NanoFramework/GeneratedCode/Quantities/ReactiveEnergy.g.cs
@@ -26,6 +26,10 @@ namespace UnitsNet
     /// <summary>
     ///     The Volt-ampere reactive hour (expressed as varh) is the reactive power of one Volt-ampere reactive produced in one hour.
     /// </summary>
+    /// <remarks>
+    ///     <see cref="ReactiveEnergy" /> has been renamed to <see cref="ElectricReactiveEnergy" />, and will be removed in a later major version.
+    /// </remarks>
+    [Obsolete("ReactiveEnergy has been renamed to ElectricReactiveEnergy, and will be removed in a later major version.")]
     public struct  ReactiveEnergy
     {
         /// <summary>

commit b3f04ec5b808d2f0abe29e5206349b5c42ebe984

💚Fix nanoFramework build, replace <see cref=""> with <c> in xmldoc (#1478)
Regression in #1444 

nanoFramework is built differently and each quantity is its own
assembly, with no reference to other quantities. This gave a build
warning, that is treated as build error.

UnitsNet.NanoFramework/GeneratedCode/Quantities/ApparentEnergy.g.cs
@@ -27,7 +27,7 @@ namespace UnitsNet
     ///     A unit for expressing the integral of apparent power over time, equal to the product of 1 volt-ampere and 1 hour, or to 3600 joules.
     /// </summary>
     /// <remarks>
-    ///     <see cref="ApparentEnergy" /> has been renamed to <see cref="ElectricApparentEnergy" />, and will be removed in a later major version.
+    ///     <c>ApparentEnergy</c> has been renamed to <c>ElectricApparentEnergy</c>, and will be removed in a later major version.
     /// </remarks>
     [Obsolete("ApparentEnergy has been renamed to ElectricApparentEnergy, and will be removed in a later major version.")]
     public struct  ApparentEnergy

UnitsNet.NanoFramework/GeneratedCode/Quantities/ApparentPower.g.cs
@@ -27,7 +27,7 @@ namespace UnitsNet
     ///     Power engineers measure apparent power as the magnitude of the vector sum of active and reactive power. Apparent power is the product of the root-mean-square of voltage and current.
     /// </summary>
     /// <remarks>
-    ///     <see cref="ApparentPower" /> has been renamed to <see cref="ElectricApparentPower" />, and will be removed in a later major version.
+    ///     <c>ApparentPower</c> has been renamed to <c>ElectricApparentPower</c>, and will be removed in a later major version.
     /// </remarks>
     [Obsolete("ApparentPower has been renamed to ElectricApparentPower, and will be removed in a later major version.")]
     public struct  ApparentPower

UnitsNet.NanoFramework/GeneratedCode/Quantities/Capacitance.g.cs
@@ -27,7 +27,7 @@ namespace UnitsNet
     ///     Capacitance is the ability of a body to store an electric charge.
     /// </summary>
     /// <remarks>
-    ///     <see cref="Capacitance" /> has been renamed to <see cref="ElectricCapacitance" />, and will be removed in a later major version.
+    ///     <c>Capacitance</c> has been renamed to <c>ElectricCapacitance</c>, and will be removed in a later major version.
     /// </remarks>
     [Obsolete("Capacitance has been renamed to ElectricCapacitance, and will be removed in a later major version.")]
     public struct  Capacitance

UnitsNet.NanoFramework/GeneratedCode/Quantities/ElectricCurrent.g.cs
@@ -27,7 +27,7 @@ namespace UnitsNet
     ///     An electric current is a flow of electric charge. In electric circuits this charge is often carried by moving electrons in a wire. It can also be carried by ions in an electrolyte, or by both ions and electrons such as in a plasma.
     /// </summary>
     /// <remarks>
-    ///     If you want to map more parameters into the <see cref="ElectricCurrent" /> class (amps RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
+    ///     If you want to map more parameters into the <c>ElectricCurrent</c> class (amps RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
     /// </remarks>
     public struct  ElectricCurrent
     {

UnitsNet.NanoFramework/GeneratedCode/Quantities/ElectricPotential.g.cs
@@ -27,7 +27,7 @@ namespace UnitsNet
     ///     In classical electromagnetism, the electric potential (a scalar quantity denoted by Φ, ΦE or V and also called the electric field potential or the electrostatic potential) at a point is the amount of electric potential energy that a unitary point charge would have when located at that point.
     /// </summary>
     /// <remarks>
-    ///     If you want to map more parameters into the <see cref="ElectricPotential" /> class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
+    ///     If you want to map more parameters into the <c>ElectricPotential</c> class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
     /// </remarks>
     public struct  ElectricPotential
     {

UnitsNet.NanoFramework/GeneratedCode/Quantities/ElectricPotentialAc.g.cs
@@ -27,7 +27,7 @@ namespace UnitsNet
     ///     The Electric Potential of a system known to use Alternating Current.
     /// </summary>
     /// <remarks>
-    ///     <see cref="ElectricPotentialAc" /> has been merged into <see cref="ElectricPotential" />, and will be removed in a later major version. If you want to map more parameters into the <see cref="ElectricPotential" /> class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
+    ///     <c>ElectricPotentialAc</c> has been merged into <c>ElectricPotential</c>, and will be removed in a later major version. If you want to map more parameters into the <c>ElectricPotential</c> class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
     /// </remarks>
     [Obsolete("ElectricPotentialAc has been merged into ElectricPotential, and will be removed in a later major version. If you want to map more parameters into the ElectricPotential class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.")]
     public struct  ElectricPotentialAc

UnitsNet.NanoFramework/GeneratedCode/Quantities/ElectricPotentialDc.g.cs
@@ -27,7 +27,7 @@ namespace UnitsNet
     ///     The Electric Potential of a system known to use Direct Current.
     /// </summary>
     /// <remarks>
-    ///     <see cref="ElectricPotentialDc" /> has been merged into <see cref="ElectricPotential" />, and will be removed in a later major version. If you want to map more parameters into the <see cref="ElectricPotential" /> class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
+    ///     <c>ElectricPotentialDc</c> has been merged into <c>ElectricPotential</c>, and will be removed in a later major version. If you want to map more parameters into the <c>ElectricPotential</c> class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
     /// </remarks>
     [Obsolete("ElectricPotentialDc has been merged into ElectricPotential, and will be removed in a later major version. If you want to map more parameters into the ElectricPotential class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.")]
     public struct  ElectricPotentialDc

UnitsNet.NanoFramework/GeneratedCode/Quantities/ReactiveEnergy.g.cs
@@ -27,7 +27,7 @@ namespace UnitsNet
     ///     The Volt-ampere reactive hour (expressed as varh) is the reactive power of one Volt-ampere reactive produced in one hour.
     /// </summary>
     /// <remarks>
-    ///     <see cref="ReactiveEnergy" /> has been renamed to <see cref="ElectricReactiveEnergy" />, and will be removed in a later major version.
+    ///     <c>ReactiveEnergy</c> has been renamed to <c>ElectricReactiveEnergy</c>, and will be removed in a later major version.
     /// </remarks>
     [Obsolete("ReactiveEnergy has been renamed to ElectricReactiveEnergy, and will be removed in a later major version.")]
     public struct  ReactiveEnergy

UnitsNet.NanoFramework/GeneratedCode/Quantities/ReactivePower.g.cs
@@ -27,7 +27,7 @@ namespace UnitsNet
     ///     Volt-ampere reactive (var) is a unit by which reactive power is expressed in an AC electric power system. Reactive power exists in an AC circuit when the current and voltage are not in phase.
     /// </summary>
     /// <remarks>
-    ///     <see cref="ReactivePower" /> has been renamed to <see cref="ElectricReactivePower" />, and will be removed in a later major version.
+    ///     <c>ReactivePower</c> has been renamed to <c>ElectricReactivePower</c>, and will be removed in a later major version.
     /// </remarks>
     [Obsolete("ReactivePower has been renamed to ElectricReactivePower, and will be removed in a later major version.")]
     public struct  ReactivePower

UnitsNet.WindowsRuntimeComponent.sln.DotSettings
@@ -1,8 +1,0 @@
-﻿<wpf:ResourceDictionary xml:space="preserve" xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml" xmlns:s="clr-namespace:System;assembly=mscorlib" xmlns:ss="urn:shemas-jetbrains-com:settings-storage-xaml" xmlns:wpf="http://schemas.microsoft.com/winfx/2006/xaml/presentation">
-	<s:Boolean x:Key="/Default/CodeInspection/CodeAnnotations/NamespacesWithAnnotations/=JetBrains_002EAnnotations/@EntryIndexedValue">True</s:Boolean>
-	<s:String x:Key="/Default/CodeStyle/CodeCleanup/Profiles/=Full/@EntryIndexedValue">&lt;?xml version="1.0" encoding="utf-16"?&gt;&lt;Profile name="Full"&gt;&lt;CSUseVar&gt;&lt;BehavourStyle&gt;CAN_CHANGE_BOTH&lt;/BehavourStyle&gt;&lt;LocalVariableStyle&gt;IMPLICIT_WHEN_INITIALIZER_HAS_TYPE&lt;/LocalVariableStyle&gt;&lt;ForeachVariableStyle&gt;ALWAYS_EXPLICIT&lt;/ForeachVariableStyle&gt;&lt;/CSUseVar&gt;&lt;CSUpdateFileHeader&gt;True&lt;/CSUpdateFileHeader&gt;&lt;CSOptimizeUsings&gt;&lt;OptimizeUsings&gt;True&lt;/OptimizeUsings&gt;&lt;EmbraceInRegion&gt;False&lt;/EmbraceInRegion&gt;&lt;RegionName&gt;&lt;/RegionName&gt;&lt;/CSOptimizeUsings&gt;&lt;CSShortenReferences&gt;True&lt;/CSShortenReferences&gt;&lt;CSReformatCode&gt;True&lt;/CSReformatCode&gt;&lt;CSharpFormatDocComments&gt;True&lt;/CSharpFormatDocComments&gt;&lt;CSReorderTypeMembers&gt;True&lt;/CSReorderTypeMembers&gt;&lt;CSRemoveCodeRedundancies&gt;True&lt;/CSRemoveCodeRedundancies&gt;&lt;AspOptimizeRegisterDirectives&gt;True&lt;/AspOptimizeRegisterDirectives&gt;&lt;HtmlReformatCode&gt;True&lt;/HtmlReformatCode&gt;&lt;CSArrangeThisQualifier&gt;True&lt;/CSArrangeThisQualifier&gt;&lt;RemoveCodeRedundancies&gt;True&lt;/RemoveCodeRedundancies&gt;&lt;CSUseAutoProperty&gt;True&lt;/CSUseAutoProperty&gt;&lt;CSMakeFieldReadonly&gt;True&lt;/CSMakeFieldReadonly&gt;&lt;JsInsertSemicolon&gt;True&lt;/JsInsertSemicolon&gt;&lt;JsReformatCode&gt;True&lt;/JsReformatCode&gt;&lt;JsFormatDocComments&gt;True&lt;/JsFormatDocComments&gt;&lt;CssAlphabetizeProperties&gt;True&lt;/CssAlphabetizeProperties&gt;&lt;CssReformatCode&gt;True&lt;/CssReformatCode&gt;&lt;XMLReformatCode&gt;True&lt;/XMLReformatCode&gt;&lt;VBOptimizeImports&gt;True&lt;/VBOptimizeImports&gt;&lt;VBShortenReferences&gt;True&lt;/VBShortenReferences&gt;&lt;VBReformatCode&gt;True&lt;/VBReformatCode&gt;&lt;VBFormatDocComments&gt;True&lt;/VBFormatDocComments&gt;&lt;/Profile&gt;</s:String>
-	<s:String x:Key="/Default/CodeStyle/CSharpVarKeywordUsage/ForBuiltInTypes/@EntryValue">UseVarWhenEvident</s:String>
-	<s:String x:Key="/Default/CodeStyle/CSharpVarKeywordUsage/ForOtherTypes/@EntryValue">UseVarWhenEvident</s:String>
-	<s:String x:Key="/Default/CodeStyle/CSharpVarKeywordUsage/ForSimpleTypes/@EntryValue">UseVarWhenEvident</s:String>
-	<s:String x:Key="/Default/CodeStyle/FileHeader/FileHeaderText/@EntryValue">Licensed under MIT No Attribution, see LICENSE file at the root.&#xD;Copyright 2013 Andreas Gullberg Larsen (andreas.larsen84@gmail.com). Maintained at https://github.com/angularsen/UnitsNet.</s:String>
-</wpf:ResourceDictionary>

UnitsNet/GeneratedCode/Quantities/ApparentEnergy.g.cs
@@ -37,7 +37,7 @@ namespace UnitsNet
     ///     A unit for expressing the integral of apparent power over time, equal to the product of 1 volt-ampere and 1 hour, or to 3600 joules.
     /// </summary>
     /// <remarks>
-    ///     <see cref="ApparentEnergy" /> has been renamed to <see cref="ElectricApparentEnergy" />, and will be removed in a later major version.
+    ///     <c>ApparentEnergy</c> has been renamed to <c>ElectricApparentEnergy</c>, and will be removed in a later major version.
     /// </remarks>
     [Obsolete("ApparentEnergy has been renamed to ElectricApparentEnergy, and will be removed in a later major version.")]
     [DataContract]

UnitsNet/GeneratedCode/Quantities/ApparentPower.g.cs
@@ -37,7 +37,7 @@ namespace UnitsNet
     ///     Power engineers measure apparent power as the magnitude of the vector sum of active and reactive power. Apparent power is the product of the root-mean-square of voltage and current.
     /// </summary>
     /// <remarks>
-    ///     <see cref="ApparentPower" /> has been renamed to <see cref="ElectricApparentPower" />, and will be removed in a later major version.
+    ///     <c>ApparentPower</c> has been renamed to <c>ElectricApparentPower</c>, and will be removed in a later major version.
     /// </remarks>
     [Obsolete("ApparentPower has been renamed to ElectricApparentPower, and will be removed in a later major version.")]
     [DataContract]

UnitsNet/GeneratedCode/Quantities/Capacitance.g.cs
@@ -37,7 +37,7 @@ namespace UnitsNet
     ///     Capacitance is the ability of a body to store an electric charge.
     /// </summary>
     /// <remarks>
-    ///     <see cref="Capacitance" /> has been renamed to <see cref="ElectricCapacitance" />, and will be removed in a later major version.
+    ///     <c>Capacitance</c> has been renamed to <c>ElectricCapacitance</c>, and will be removed in a later major version.
     /// </remarks>
     [Obsolete("Capacitance has been renamed to ElectricCapacitance, and will be removed in a later major version.")]
     [DataContract]

UnitsNet/GeneratedCode/Quantities/ElectricCurrent.g.cs
@@ -37,7 +37,7 @@ namespace UnitsNet
     ///     An electric current is a flow of electric charge. In electric circuits this charge is often carried by moving electrons in a wire. It can also be carried by ions in an electrolyte, or by both ions and electrons such as in a plasma.
     /// </summary>
     /// <remarks>
-    ///     If you want to map more parameters into the <see cref="ElectricCurrent" /> class (amps RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
+    ///     If you want to map more parameters into the <c>ElectricCurrent</c> class (amps RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
     /// </remarks>
     [DataContract]
     [DebuggerTypeProxy(typeof(QuantityDisplay))]

UnitsNet/GeneratedCode/Quantities/ElectricPotential.g.cs
@@ -37,7 +37,7 @@ namespace UnitsNet
     ///     In classical electromagnetism, the electric potential (a scalar quantity denoted by Φ, ΦE or V and also called the electric field potential or the electrostatic potential) at a point is the amount of electric potential energy that a unitary point charge would have when located at that point.
     /// </summary>
     /// <remarks>
-    ///     If you want to map more parameters into the <see cref="ElectricPotential" /> class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
+    ///     If you want to map more parameters into the <c>ElectricPotential</c> class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
     /// </remarks>
     [DataContract]
     [DebuggerTypeProxy(typeof(QuantityDisplay))]

UnitsNet/GeneratedCode/Quantities/ElectricPotentialAc.g.cs
@@ -37,7 +37,7 @@ namespace UnitsNet
     ///     The Electric Potential of a system known to use Alternating Current.
     /// </summary>
     /// <remarks>
-    ///     <see cref="ElectricPotentialAc" /> has been merged into <see cref="ElectricPotential" />, and will be removed in a later major version. If you want to map more parameters into the <see cref="ElectricPotential" /> class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
+    ///     <c>ElectricPotentialAc</c> has been merged into <c>ElectricPotential</c>, and will be removed in a later major version. If you want to map more parameters into the <c>ElectricPotential</c> class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
     /// </remarks>
     [Obsolete("ElectricPotentialAc has been merged into ElectricPotential, and will be removed in a later major version. If you want to map more parameters into the ElectricPotential class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.")]
     [DataContract]

UnitsNet/GeneratedCode/Quantities/ElectricPotentialDc.g.cs
@@ -37,7 +37,7 @@ namespace UnitsNet
     ///     The Electric Potential of a system known to use Direct Current.
     /// </summary>
     /// <remarks>
-    ///     <see cref="ElectricPotentialDc" /> has been merged into <see cref="ElectricPotential" />, and will be removed in a later major version. If you want to map more parameters into the <see cref="ElectricPotential" /> class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
+    ///     <c>ElectricPotentialDc</c> has been merged into <c>ElectricPotential</c>, and will be removed in a later major version. If you want to map more parameters into the <c>ElectricPotential</c> class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.
     /// </remarks>
     [Obsolete("ElectricPotentialDc has been merged into ElectricPotential, and will be removed in a later major version. If you want to map more parameters into the ElectricPotential class (volts RMS, phase angle, etc.), create your own wrapper type such as a record or named tuple.")]
     [DataContract]

UnitsNet/GeneratedCode/Quantities/ReactiveEnergy.g.cs
@@ -37,7 +37,7 @@ namespace UnitsNet
     ///     The Volt-ampere reactive hour (expressed as varh) is the reactive power of one Volt-ampere reactive produced in one hour.
     /// </summary>
     /// <remarks>
-    ///     <see cref="ReactiveEnergy" /> has been renamed to <see cref="ElectricReactiveEnergy" />, and will be removed in a later major version.
+    ///     <c>ReactiveEnergy</c> has been renamed to <c>ElectricReactiveEnergy</c>, and will be removed in a later major version.
     /// </remarks>
     [Obsolete("ReactiveEnergy has been renamed to ElectricReactiveEnergy, and will be removed in a later major version.")]
     [DataContract]

UnitsNet/GeneratedCode/Quantities/ReactivePower.g.cs
@@ -37,7 +37,7 @@ namespace UnitsNet
     ///     Volt-ampere reactive (var) is a unit by which reactive power is expressed in an AC electric power system. Reactive power exists in an AC circuit when the current and voltage are not in phase.
     /// </summary>
     /// <remarks>
-    ///     <see cref="ReactivePower" /> has been renamed to <see cref="ElectricReactivePower" />, and will be removed in a later major version.
+    ///     <c>ReactivePower</c> has been renamed to <c>ElectricReactivePower</c>, and will be removed in a later major version.
     /// </remarks>
     [Obsolete("ReactivePower has been renamed to ElectricReactivePower, and will be removed in a later major version.")]
     [DataContract]