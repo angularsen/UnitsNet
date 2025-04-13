e421a1b1090e8187896154b62577d21df304467e
lipchev <lipchev@gmail.com>
2025-04-13 16:12:06 +0300

    adding the git-ignored files

---
+++ UnitsNet/CustomCode/Debug/UnitAbbreviation.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+
+namespace UnitsNet.Debug;
+
+/// <summary>
+///     Represents the display information for unit abbreviations associated with a specific quantity.
+/// </summary>
+/// <remarks>
+///     This struct provides access to default and parseable abbreviations for units, as well as the default abbreviation
+///     for the current unit of the quantity.
+/// </remarks>
+[DebuggerDisplay("{DefaultAbbreviation}")]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public readonly struct UnitAbbreviation
+{
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private readonly QuantityDebugProxy _quantityProxy;
+
+    internal UnitAbbreviation(QuantityDebugProxy quantityProxy)
+    {
+        _quantityProxy = quantityProxy;
+        Abbreviations = quantityProxy.Configuration.UnitAbbreviations.GetUnitAbbreviations(quantityProxy.UnitKey, QuantityDebugProxy.DefaultFormatProvider);
+    }
+
+    /// <summary>
+    ///     Gets the default abbreviation for the current unit of the quantity.
+    /// </summary>
+    /// <remarks>
+    ///     The default abbreviation is determined based on the unit key of the quantity and the configured unit abbreviations
+    ///     cache.
+    /// </remarks>
+    /// <value>
+    ///     A <see cref="string" /> representing the default abbreviation for the current unit.
+    /// </value>
+    /// <exception cref="UnitNotFoundException">
+    ///     Thrown when no unit information is found for the specified unit key.
+    /// </exception>
+    /// <exception cref="InvalidOperationException">
+    ///     Thrown when no abbreviations are mapped for the specified unit.
+    /// </exception>
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    public string DefaultAbbreviation
+    {
+        get => Abbreviations[0];
+    }
+
+    /// <summary>
+    ///     Gets a read-only list of unit abbreviations associated with the current quantity.
+    /// </summary>
+    /// <remarks>
+    ///     This property provides all abbreviations that can represent the unit of the current quantity.
+    ///     These abbreviations are culture-sensitive and may vary depending on the localization settings.
+    /// </remarks>
+    /// <value>
+    ///     A read-only list of strings representing the unit abbreviations.
+    /// </value>
+    /// <exception cref="UnitNotFoundException">
+    ///     Thrown when no unit information is found for the specified unit key.
+    /// </exception>
+    [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+    public IReadOnlyList<string> Abbreviations { get; }
+
+    /// <summary>
+    ///     Gets the collection of parseable unit abbreviations for the associated quantity.
+    /// </summary>
+    /// <remarks>
+    ///     This property provides an array of <see cref="UnitsNet.Debug.UnitAbbreviations" /> objects,
+    ///     each representing the abbreviations that can be parsed for a specific unit of the quantity.
+    /// </remarks>
+    public UnitAbbreviations[] UnitAbbreviations
+    {
+        get
+        {
+            QuantityDebugProxy quantityProxy = _quantityProxy;
+            return quantityProxy.QuantityInfo.UnitInfos.Select(targetUnit => new UnitAbbreviations(quantityProxy, targetUnit)).ToArray();
+        }
+    }
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return DefaultAbbreviation;
+    }
+}
+
+/// <summary>
+///     Represents a collection of unit abbreviations for a specific unit of a quantity.
+/// </summary>
+/// <remarks>
+///     This struct provides access to the unit's abbreviations and allows conversion of the quantity to the specified
+///     unit.
+/// </remarks>
+[DebuggerDisplay("{ToString(), nq}")]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public readonly struct UnitAbbreviations
+{
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private QuantityDebugProxy QuantityProxy { get; }
+
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private UnitInfo TargetUnit { get; }
+
+    internal UnitAbbreviations(QuantityDebugProxy quantityProxy, UnitInfo targetUnit)
+    {
+        QuantityProxy = quantityProxy;
+        TargetUnit = targetUnit;
+    }
+
+    /// <summary>
+    ///     Gets a debug proxy representation of the quantity associated with the current unit.
+    /// </summary>
+    /// <remarks>
+    ///     This property provides a converted view of the quantity in the context of the specified unit,
+    ///     enabling detailed inspection of its components during debugging.
+    /// </remarks>
+    [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+    public QuantityDebugProxy Quantity
+    {
+        get => QuantityProxy.ConvertTo(TargetUnit);
+    }
+
+    /// <summary>
+    ///     Gets the list of abbreviations associated with the target unit.
+    /// </summary>
+    /// <remarks>
+    ///     The abbreviations are retrieved from the <see cref="UnitAbbreviationsCache" /> configured in the
+    ///     <see cref="QuantityDebugProxy" />. These abbreviations are culture-sensitive and may vary based on the
+    ///     current culture or a fallback culture.
+    /// </remarks>
+    /// <value>
+    ///     A read-only list of strings representing the abbreviations for the target unit.
+    /// </value>
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    public IReadOnlyList<string> Abbreviations
+    {
+        get => QuantityProxy.Configuration.UnitAbbreviations.GetAbbreviations(TargetUnit, QuantityDebugProxy.DefaultFormatProvider);
+    }
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return string.Join(", ", Abbreviations.Select(x => $"\"{x}\""));
+    }
+}
---
+++ UnitsNet/CustomCode/Debug/UnitInformation.cs
@@ -0,0 +1,107 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+
+namespace UnitsNet.Debug;
+
+/// <summary>
+///     Represents information about a specific unit within a quantity.
+/// </summary>
+/// <remarks>
+///     This struct provides details about a unit, including its definition and possible conversions.
+///     It is used to encapsulate unit-related metadata and operations within the context of a quantity.
+/// </remarks>
+[DebuggerDisplay("{Definition.Name, nq}")]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public readonly struct UnitInformation
+{
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private readonly QuantityDebugProxy _quantityProxy;
+
+    internal UnitInformation(QuantityDebugProxy quantityProxy, UnitInfo unitInfo)
+    {
+        _quantityProxy = quantityProxy;
+        Definition = unitInfo;
+    }
+
+    /// <summary>
+    ///     Gets the definition of the unit, including its metadata and associated details.
+    /// </summary>
+    /// <remarks>
+    ///     This property provides access to the unit's definition, which includes its name, value, and other metadata.
+    ///     It is useful for retrieving information about the specific unit represented within the context of a quantity.
+    /// </remarks>
+    // [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+    public UnitInfo Definition { get; }
+
+    /// <summary>
+    ///     Gets the collection of unit conversions associated with the current unit.
+    /// </summary>
+    /// <remarks>
+    ///     Each <see cref="UnitConversion" /> in the collection represents a possible conversion
+    ///     from the current unit to another unit within the same quantity.
+    ///     This property is useful for enumerating all available conversions for a unit.
+    /// </remarks>
+    public UnitConversion[] UnitConversions
+    {
+        get
+        {
+            QuantityDebugProxy quantityProxy = _quantityProxy;
+            return quantityProxy.QuantityInfo.UnitInfos.Select(unit => new UnitConversion(quantityProxy, unit)).ToArray();
+        }
+    }
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return Definition.ToString();
+    }
+}
+
+/// <summary>
+///     Represents a unit conversion for a specific quantity and unit type.
+/// </summary>
+/// <remarks>
+///     This struct provides functionality to convert a quantity to a specific unit and retrieve its display
+///     representation.
+///     It is primarily used internally within the <see cref="QuantityDebugProxy" /> struct.
+/// </remarks>
+[DebuggerDisplay("{TargetUnit.Value, nq}")]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public readonly struct UnitConversion
+{
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private QuantityDebugProxy QuantityProxy { get; }
+
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private UnitInfo TargetUnit { get; }
+
+    internal UnitConversion(QuantityDebugProxy quantityProxy, UnitInfo targetUnit)
+    {
+        QuantityProxy = quantityProxy;
+        TargetUnit = targetUnit;
+    }
+
+    /// <summary>
+    ///     Gets the quantity representation after converting to the specified unit.
+    /// </summary>
+    /// <remarks>
+    ///     This property provides a <see cref="QuantityDebugProxy" /> instance that represents the quantity
+    ///     converted to the unit specified by the <see cref="UnitConversion" />.
+    ///     It utilizes the <see cref="UnitConverter" /> for the conversion process.
+    /// </remarks>
+    /// <value>
+    ///     A <see cref="QuantityDebugProxy" /> instance representing the converted quantity.
+    /// </value>
+    [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+    public QuantityDebugProxy Quantity
+    {
+        get => QuantityProxy.ConvertTo(TargetUnit);
+    }
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return TargetUnit.ToString();
+    }
+}
