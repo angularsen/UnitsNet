e0bb45a0188a6d05b6014b58278d4f30793f2379
Andrew Moskevitz <49752377+Applesauce314@users.noreply.github.com>
2026-05-02 13:38:50 -0400

    Update Length.extra.cs
    fixed using `Feet` instead of local copy `feet`

--- UnitsNet/CustomCode/Quantities/Length.extra.cs
+++ UnitsNet/CustomCode/Quantities/Length.extra.cs
@@ -250,7 +250,7 @@ namespace UnitsNet
 
             inchPart.Append('"');
 
-            if (Feet == 0)
+            if (feet == 0)
             {
                 return inchPart.ToString();
             }
