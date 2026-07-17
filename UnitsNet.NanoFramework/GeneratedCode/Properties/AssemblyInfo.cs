950a0e66758329d0df1c5fdafc93031f8febafa7
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2026-07-18 01:12:16 +0200

    UnitsNet: 5.75.1

--- UnitsNet.NanoFramework/GeneratedCode/Properties/AssemblyInfo.cs
+++ UnitsNet.NanoFramework/GeneratedCode/Properties/AssemblyInfo.cs
@@ -47,8 +47,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("5.75.0")]
-[assembly: AssemblyFileVersion("5.75.0")]
+[assembly: AssemblyVersion("5.75.1")]
+[assembly: AssemblyFileVersion("5.75.1")]
 
 //////////////////////////////////////////////////
 // This assembly doens't require native support //
